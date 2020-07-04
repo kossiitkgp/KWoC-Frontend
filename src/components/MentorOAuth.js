03e08dba5b017c0898b0ea0fa9743138bed61173
rakaar <rka87338@gmail.com>
2020-07-05 00:00:13 +0530

    Extended code for Mentor Oauth

--- src/components/MentorOAuth.js
+++ src/components/MentorOAuth.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import axios from 'axios';
 
 export default function MentorOAuth(){
     
@@ -6,6 +7,19 @@ export default function MentorOAuth(){
         const params = new URLSearchParams(window.location.search)
         const code = params.get('code')
         console.log("Send this to backend ", code)
+        const URL = "http://localhost:5000/mentor/oauth"
+        const data = {
+            "code": code,
+            "state": "PAKKA RANDOM"
+        }
+        axios
+            .post(URL, data)
+            .then(res => {
+                console.log("res.data",res.data)
+            })
+            .catch(err => {
+                console.log("err is ",err)
+            })
     },[])
     
     return (
