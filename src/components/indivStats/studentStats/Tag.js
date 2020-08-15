78d0a0b814a7411468d51e9f5cf17e196d63e085
rakaar <rka87338@gmail.com>
2020-08-15 18:13:15 +0530

    Added more styles in Tags in Student Stats

--- src/components/indivStats/studentStats/Tag.js
+++ src/components/indivStats/studentStats/Tag.js
@@ -2,13 +2,16 @@ import React from 'react'
 
 export default function Tag(props) {
 
-    const TAG_TYPES = ['primary', 'link', 'info', 'success', 'warning', 'danger']
+    const TAG_TYPES = ['is-primary', 'is-link', 'is-info', 'is-success', 'is-warning', 'is-danger',
+                        'is-black', 'is-dark', 'is-light','is-primary is-light', 'is-link is-light',
+                        'is-info is-light', 'is-success is-light', 'is-warning is-light','is-danger is-light'
+                    ]
     const LEN = TAG_TYPES.length
     
     return(
         <span 
         style = {{ margin: '5px' }}
-        className={`tag is-${TAG_TYPES[Math.floor(Math.random()*LEN)]} is-medium`}>
+        className={`tag ${TAG_TYPES[Math.floor(Math.random()*LEN)]} is-medium`}>
         {props.content}
         </span>
     )
