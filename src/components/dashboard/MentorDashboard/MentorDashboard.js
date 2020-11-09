918fe104985c1f708368e519f9d2445e37a208a5
Yash Sharma <yashrsharma44@gmail.com>
2020-11-09 12:07:07 +0530

    Added some more changes
    Signed-off-by: Yash Sharma <yashrsharma44@gmail.com>

--- src/components/dashboard/MentorDashboard/MentorDashboard.js
+++ src/components/dashboard/MentorDashboard/MentorDashboard.js
@@ -1,40 +1,39 @@
-import React, { useEffect } from 'react'
-import { BACKEND_URL } from '../../../constants/constants'
-import './MentorDashboard.scss'
+import React, { useEffect } from 'react';
+import { BACKEND_URL } from '../../../constants/constants';
+import './MentorDashboard.scss';
 
 export default function MentorDashboard() {
+  useEffect(() => {
+    const URL = `${BACKEND_URL}/dashboard/mentor`;
+    const data = {
+      username: localStorage.getItem('mentor_username'),
+      jwt: localStorage.getItem('mentor_jwt'),
+    };
+    // using the jwt and username to fetch details for Dashboard
+    // fetch(URL, {
+    //     method: 'POST',
+    //     data: JSON.stringify(data)
+    // })
+    // .then(res => {
+    //     console.log("res.data ", res.data)
+    // })
+    // .catch(err => {
+    //     if(err.response.status === 400)
+    //         alert("Invalid Request")
+    //     else
+    //         alert("Server Error, Please try again")
+    // })
+  }, []);
 
-    useEffect(() => {
-        const URL = `${BACKEND_URL}/dashboard/mentor`
-        const data = {
-            "username": localStorage.getItem("mentor_username"),
-            "jwt": localStorage.getItem("mentor_jwt")
-        }
-        // using the jwt and username to fetch details for Dashboard
-        fetch(URL, {
-            method: 'POST',
-            data: JSON.stringify(data)
-        })
-        .then(res => {
-            console.log("res.data ", res.data)
-        })
-        .catch(err => {
-            if(err.response.status === 400)
-                alert("Invalid Request")
-            else 
-                alert("Server Error, Please try again")
-        })
-    }, [])
-
-    return (
-        <div>
-            {/* Mentor Dashboard here
+  return (
+    <div>
+      {/* Mentor Dashboard here
             Plans to include the following
              -> Useful links - how to write README, others if any?
              -> Important Announcements
              -> Stats of indiv Mentor ???
             */}
-            <h1>Mentor Dashboard</h1>
-        </div>
-    )
+      <h1>Mentor Dashboard</h1>
+    </div>
+  );
 }
