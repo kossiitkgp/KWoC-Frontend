bfd5399f50dc02f4b4b0bbdad962f7e919653277
Yash Sharma <yashrsharma44@gmail.com>
2020-11-26 12:53:13 +0530

    uncommented the usestate function
    Signed-off-by: Yash Sharma <yashrsharma44@gmail.com>

--- src/components/dashboard/MentorDashboard/MentorDashboard.js
+++ src/components/dashboard/MentorDashboard/MentorDashboard.js
@@ -92,21 +92,21 @@ export default function MentorDashboard() {
     const data = {
       username: localStorage.getItem('mentor_username'),
     };
-    // console.log('data is ', data);
-    // fetch(URL, {
-    //   method: 'POST',
-    //   body: JSON.stringify(data),
-    // })
-    //   .then((res) => res.json())
-    //   .then((res) => {
-    //     console.log('res is  ', res);
-    //     setFullName(res.name);
-    //     setProjects(res.projects);
-    //   })
-    //   .catch((err) => {
-    //     console.log('err is ', err);
-    //     alert('Server Error, Please try again');
-    //   });
+    console.log('data is ', data);
+    fetch(URL, {
+      method: 'POST',
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log('res is  ', res);
+        setFullName(res.name);
+        setProjects(res.projects);
+      })
+      .catch((err) => {
+        console.log('err is ', err);
+        alert('Server Error, Please try again');
+      });
   }, []);
 
   // sample data kept for future reference
