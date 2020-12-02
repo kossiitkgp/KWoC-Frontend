0c99cd82bd3e27aa400cbaedbfb83e65a40980fc
Sahil Shubham <sahil.shubham2000@gmail.com>
2020-12-02 20:58:50 +0530

    made project card structure;

--- src/components/projects/ProjectCard.js
+++ src/components/projects/ProjectCard.js
@@ -4,36 +4,52 @@ import '../../styles/projects.scss';
 
 export default function Card(props) {
   return (
-    <div class="is-centered" className='about' >
-      <div class="card is-one-quarter">
-        <header class="card-header has-background-link has-text-white">
-            <p class="card-header-title has-text-white">
-              {props.name}
+    <div className='card'>
+      <header className='card-header has-background-link has-text-white'>
+        <p className='card-header-title has-text-white is-centered'>
+          {props.name}
+        </p>
+      </header>
+      <div className='card-content'>
+        <div className='content'>{props.desc}</div>
+        <br />
+        <section className='tags' id='projectTags'>
+          <div className='container'>
+            <p>
+              {props.tags.map((tag) => (
+                <span key={tag} className='tag cust-tag is-dark is-medium'>
+                  {tag}
+                </span>
+              ))}
             </p>
-            <a href="#" class="card-header-icon" aria-label="more options">
-               <span class="icon">
-                <i class="fas fa-angle-down" aria-hidden="true"></i>
-              </span>
-             </a>
-        </header>
-        <div class="card-content">
-            <div class="content">
-                {props.desc}
-                <br/><br/><br/><br/>
-            </div>
-            <div>
-                Mentored by: {props.mentor}
-            </div>
-        </div>
-        <div class="card-content tags columns">
-            {props.tags.map(tag => (
-              <span class="tag is-warning is-rounded column">{tag}</span>
-            ))}
-        </div>
-        <footer class="card-footer is-centered">
-        <button class="footer-btn button is-info is-medium is-fullwidth">Details</button>
-        </footer>
+          </div>
+        </section>
+        <br />
+        <p>
+          Mentored by: {props.mentor}
+          <br /> ({props.mentorId})
+        </p>
+        <p>
+          Communication Channel:
+          <a
+            className='footer-btn button is-link is-outlined'
+            href={props.commLink}
+            target='_blank'
+          >
+            Join Now
+          </a>
+        </p>
       </div>
+
+      <footer className='card-footer is-centered'>
+        <a
+          className='footer-btn button is-primary is-medium is-fullwidth'
+          href={props.projectLink}
+          target='_blank'
+        >
+          View Project
+        </a>
+      </footer>
     </div>
   );
 }
