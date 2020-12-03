8a6e04745662582609872c5170a1f61a0ce782e9
Mukul Mehta <mukul.csiitkgp@gmail.com>
2020-12-03 18:24:22 +0530

    Update Stats API Endpoint
    Heroku was taking a lot of time to respond to a single request. Update the URL to a deployed version on an EC2 instance

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,3 @@
 export const BACKEND_URL = `https://kwoc.metamehta.me`
-export const STATS_API = `https://kwoc-stats-test-api.herokuapp.com`
+export const STATS_API = `http://stats.metamehta.me`
 export const MID_EVAL_DATE = '2020-12-18'
