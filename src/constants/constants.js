bd642f16419dd6081980e36373349e3f0096af35
Mukul Mehta <mukul.csiitkgp@gmail.com>
2020-12-03 18:26:36 +0530

    Change URL to HTTPS in constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,3 @@
 export const BACKEND_URL = `https://kwoc.metamehta.me`
-export const STATS_API = `http://stats.metamehta.me`
+export const STATS_API = `https://stats.metamehta.me`
 export const MID_EVAL_DATE = '2020-12-18'
