89d4ec1c7be07d2395d7b5b56d9b29a4619ac20c
sahil-shubham <sahil.shubham2000@gmail.com>
2021-11-05 21:29:52 +0530

    better homepage, no need for separate styling files for page sections;
    Signed-off-by: sahil-shubham <sahil.shubham2000@gmail.com>

--- src/views/Home.js
+++ src/views/Home.js
@@ -4,8 +4,14 @@ import github from "../assets/github.svg";
 import group from "../assets/people.svg";
 import Part from "../components/Part";
 import Timeline from "../components/Timeline";
+import {
+  MENTOR_MANUAL,
+  MENTOR_REGISTRATION_LINK,
+  STUDENT_MANUAL,
+  STUDENT_REGISTRATION_LINK,
+} from "../constants";
 
-export default function Home() {
+function Home() {
   const tags = [
     "Machine learning",
     "Android",
@@ -29,152 +35,118 @@ export default function Home() {
 
   return (
     <div className="home">
-      <section
-        className="hero is-fullheight is-dark is-bold"
-        style={{ position: "relative" }}
-      >
+      <section className="hero">
         <Part />
         <div className="hero-body">
-          <div className="container">
-            <h1 className="title" id="hero">
-              Welcome to KWoC
-            </h1>
+          <h1 className="title">Welcome to KWoC</h1>
 
-            <div
-              className="reg-btns "
-              style={{ padding: "0 20%", marginBottom: "20px" }}
+          <div className="btns-container">
+            <a className="button" href={STUDENT_REGISTRATION_LINK}>
+              Student Registration
+            </a>
+
+            <a className="button" href={MENTOR_REGISTRATION_LINK}>
+              Mentor Registration
+            </a>
+          </div>
+
+          <div className="btns-container">
+            <a
+              className="button"
+              href={STUDENT_MANUAL}
+              rel="noopener noreferrer"
+              target="_blank"
             >
-              <p className="control">
-                <a
-                  className="button is-fullwidth "
-                  href={`https://github.com/login/oauth/authorize?scope=user:email&client_id=${process.env.GITHUB_AUTH_CLIENT_ID}&state=student`}
-                  style={{ marginBottom: "20px" }}
-                >
-                  Student Registration
-                </a>
-              </p>
-              <p className="control">
-                <a
-                  className="button is-fullwidth "
-                  href={`https://github.com/login/oauth/authorize?scope=user:email&client_id=${process.env.GITHUB_AUTH_CLIENT_ID}&state=mentor`}
-                  style={{ marginBottom: "20px" }}
-                >
-                  Mentor Registration
-                </a>
-              </p>
-            </div>
+              Student Manual
+            </a>
 
-            <div className="manual-btns field is-grouped is-grouped-centered">
-              <p className="control">
-                <a
-                  className="button is-fullwidth"
-                  href="https://drive.google.com/file/d/1_K30Crr3vVgRS-NGFVC2obQypD22jTuq/view"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                  style={{ marginBottom: "20px" }}
-                >
-                  Student Manual
-                </a>
-              </p>
-              <p className="control">
-                <a
-                  className="button is-fullwidth "
-                  href="https://drive.google.com/file/d/1P7F-Jfi_XdUeptc4t5KM09PtTJRatjj1/view"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  Mentor Manual
-                </a>
-              </p>
-            </div>
+            <a
+              className="button"
+              href={MENTOR_MANUAL}
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Mentor Manual
+            </a>
           </div>
         </div>
       </section>
 
-      {/* <section className="about">
-				<div className="container" />
-			</section> */}
-      <section className="why-kwoc" id="about">
-        <div className="container">
-          <h1>About</h1>
-          <p>
-            Kharagpur Winter of Code is a 5-week long online program for
-            students who are new to open source software development. The
-            program not only helps students to get involved in open source, but
-            also prepares them for many open source summer programs; Google
-            Summer of Code being one of them.
-          </p>
-          <br />
-          <br />
-          <h2>Why KWoC?</h2>
+      <section className="why-kwoc container" id="about">
+        <h1>About</h1>
+        <p>
+          Kharagpur Winter of Code is a 5-week long online program for students
+          who are new to open source software development. The program not only
+          helps students to get involved in open source, but also prepares them
+          for many open source summer programs; Google Summer of Code being one
+          of them.
+        </p>
 
-          <h3>Intro to Open Source</h3>
+        <h2>Why KWoC?</h2>
 
-          <p>
-            KWoC provides a great opportunity to get acquainted with Github
-            along with Git commands and contribute to open source efficiently.
-            Brush up your coding skills
-          </p>
+        <h3>Intro to Open Source</h3>
 
-          <p>
-            If you love coding and want to learn about software development then
-            KWoC helps you to get a glimpse of it and gives you a head start.
-          </p>
-          <br />
-          <br />
-          <h3>Prepare for GSoC</h3>
+        <p>
+          KWoC provides a great opportunity to get acquainted with Github along
+          with Git commands and contribute to open source efficiently. Brush up
+          your coding skills
+        </p>
 
-          <p>
-            With KWoC, you get to know about how to select a project, interact
-            with mentors and learn all other things that prepare you in the best
-            way for the next GSoC.
-          </p>
-        </div>
-      </section>
-      <section className="tags" id="tags">
-        <div className="container">
-          <h1>Tags</h1>
+        <p>
+          If you love coding and want to learn about software development then
+          KWoC helps you to get a glimpse of it and gives you a head start.
+        </p>
+        <h3>Prepare for GSoC</h3>
 
-          <p>
-            {tags.map((tag) => (
-              <span key={tag} className="tag cust-tag is-medium">
-                {tag}
-              </span>
-            ))}
-          </p>
-        </div>
-      </section>
-      <section className="timeline" id="tline">
-        <div className="container">
-          <h1 style={{ zIndex: 10 }}>Timeline</h1>
-          <Part />
-          <Timeline />
-        </div>
+        <p>
+          With KWoC, you get to know about how to select a project, interact
+          with mentors and learn all other things that prepare you in the best
+          way for the next GSoC.
+        </p>
       </section>
 
-      <section className="stats">
-        <div className="container">
-          <h1>Statistics: KWoC 2019</h1>
-          <div className="columns is-centered">
-            <div className="column has-text-centered">
-              <img src={group} alt="Group" />
+      <section className="tags container" id="tags">
+        <h1>Tags</h1>
 
-              <h3>2000+ Participants</h3>
-            </div>
-            <div className="column has-text-centered">
-              <img src={pullRequest} alt="pullRequest" />
+        <p>
+          {tags.map((tag) => (
+            <span key={tag} className="tag">
+              {tag}
+            </span>
+          ))}
+        </p>
+      </section>
 
-              <h3>600+ Pull Requests</h3>
-            </div>
-            <div className="column has-text-centered">
-              {/* <Card heading="70+ Mentors" body="Some Random Body" /> */}
-              <img src={github} alt="Project" />
+      <section className="timeline container" id="tline">
+        <h1>Timeline</h1>
+        <Part />
+        <Timeline />
+      </section>
 
-              <h3>150+ Projects</h3>
-            </div>
+      <section className="stats container">
+        <h1>Statistics: KWoC 2019</h1>
+        <div className="columns">
+          <div className="column">
+            <img src={group} alt="Group" />
+
+            <h3>2000+ Participants</h3>
+          </div>
+
+          <div className="column">
+            <img src={pullRequest} alt="pullRequest" />
+
+            <h3>600+ Pull Requests</h3>
+          </div>
+
+          <div className="column">
+            <img src={github} alt="Project" />
+
+            <h3>150+ Projects</h3>
           </div>
         </div>
       </section>
     </div>
   );
 }
+
+export default Home;
