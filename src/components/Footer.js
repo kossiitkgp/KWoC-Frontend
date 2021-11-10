4fe382128d0714ba4f995bf49b25f687afd1bf82
chirag828049 <cghosh828049@gmail.com>
2021-11-10 09:25:42 +0530

    redesigned footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,24 +3,24 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="footer">
-      <div>
+      <div className="footer-column">
         <h1>Kharagpur Winter of Code</h1>
         <h2>
           <a href="https://kossiitkgp.org">With &#10084; by KOSS</a>
         </h2>
       </div>
 
-      <div>
+      <div className="footer-column">
         <a href="/#tline">Timeline</a>
         <a href="https://www.facebook.com/groups/kwoc2016">Social Groups</a>
       </div>
 
-      <div>
+      <div className="footer-column">
         <a href="/FAQ">FAQ</a>
         <a href="/#about">About KOSS</a>
       </div>
 
-      <div>
+      <div className="footer-column">
         <a
           target="_blank"
           rel="noreferrer noopener"
