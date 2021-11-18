8cd081f91b6c86a08f90af2691aa1dd50ed9998d
sahil-shubham <sahil.shubham2000@gmail.com>
2021-11-18 16:44:36 +0530

    background effect playground;
    Signed-off-by: sahil-shubham <sahil.shubham2000@gmail.com>

---
+++ src/components/BlurredCircles.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function BlurredCircles() {
+  return (
+    <div className="blurred-circles">
+      <div className="wrapper">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
+
+export default BlurredCircles;
