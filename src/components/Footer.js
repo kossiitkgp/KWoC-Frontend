436dc1752b2a82d13c19d12de6f21bc033c34ce6
sahil-shubham <sahil.shubham2000@gmail.com>
2021-11-19 00:42:24 +0530

    updated footer structure a bit;
    Signed-off-by: sahil-shubham <sahil.shubham2000@gmail.com>

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,31 +3,59 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="footer">
-      <div className="footer-column">
+      <nav>
+        <div className="footer-column">
+          {/* TODO: add all the links here, more if needed */}
+          <h3>Social Groups </h3>
+          <ul>
+            <li>
+              <a>Facebook</a>
+            </li>
+            <li>
+              <a>LinkedIn</a>
+            </li>
+            <li>
+              <a>Twitter</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="footer-column">
+          <h3>Quick Links</h3>
+          <ul>
+            <li>
+              <a href="/#tline">Timeline</a>
+            </li>
+            <li>
+              <a href="/FAQ">FAQ</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="footer-column">
+          <h3>Other</h3>
+          <ul>
+            <li>
+              <a href="/#about">About KOSS</a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                rel="noreferrer noopener"
+                href="https://github.com/kossiitkgp/kwoc-bugs"
+              >
+                Report a Bug!
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <div>
         <h1>Kharagpur Winter of Code</h1>
         <h2>
           <a href="https://kossiitkgp.org">With &#10084; by KOSS</a>
         </h2>
-      </div>
-
-      <div className="footer-column">
-        <a href="/#tline">Timeline</a>
-        <a href="https://www.facebook.com/groups/kwoc2016">Social Groups</a>
-      </div>
-
-      <div className="footer-column">
-        <a href="/FAQ">FAQ</a>
-        <a href="/#about">About KOSS</a>
-      </div>
-
-      <div className="footer-column">
-        <a
-          target="_blank"
-          rel="noreferrer noopener"
-          href="https://github.com/kossiitkgp/kwoc-bugs"
-        >
-          Report a Bug!
-        </a>
       </div>
     </div>
   );
