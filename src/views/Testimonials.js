3970076af17c354edc55ed20b1bd56d846f0b631
chirag828049 <cghosh828049@gmail.com>
2021-11-21 19:38:26 +0530

    removed markdown abd made styles consistent in testimonial;

--- src/views/Testimonials.js
+++ src/views/Testimonials.js
@@ -1,22 +1,7 @@
-import React, { useEffect, useState } from "react";
-import ReactMarkdown from "react-markdown";
-import gfm from "remark-gfm";
+import React from "react";
 import testimonialData from "../data/testimonials.json";
 
 export default function Testimonial() {
-  const [text, setText] = useState("");
-
-  useEffect(() => {
-    async function getText() {
-      const markdown = await fetch("/testimonials.md");
-      const text = await markdown.text();
-      console.log(text);
-
-      setText(text);
-    }
-    getText();
-  }, []);
-
   return (
     <div className="testimonial">
       <section className="hero-body">
@@ -26,7 +11,6 @@ export default function Testimonial() {
         </h2>
       </section>
       {/* TODO: maybe a markdown renderer here if time permits, reading and editing these shouldn't require writing code */}
-      <ReactMarkdown plugins={[gfm]}>{text}</ReactMarkdown>
       <section className="testimonial-list">
         {testimonialData.testimonials.map((testimonial) => (
           <div className="testimonial-box">
@@ -37,7 +21,9 @@ export default function Testimonial() {
                 <div className="testimonial-biotext">
                   <div>{testimonial.role}</div>
                 </div>
-                <a href={testimonial.blogLink}>Blog</a>
+                <a className="button" href={testimonial.blogLink}>
+                  Blog
+                </a>
               </div>
             </div>
             <div className="testimonial-desc">{testimonial.desc}</div>
