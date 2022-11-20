ad90a8c80c65c6572bd0e233a6f2627ec042293c
chirag-ghosh <cghosh828049@gmail.com>
2022-11-20 09:18:31 +0530

    Add production client ID;

--- src/constants.js
+++ src/constants.js
@@ -3,7 +3,10 @@ export const BACKEND_URL = `https://kwoc-2022.fly.dev`;
 export const STATS_API = `https://kwoc21-stats.herokuapp.com/`;
 export const MID_EVAL_DATE = "2022-12-24";
 export const END_EVAL_DATE = "2023-01-04";
-const GITHUB_AUTH_CLIENT_ID = `d46d572a8e9fe53f6c14`;
+const GITHUB_AUTH_CLIENT_ID =
+  process.env.NODE_ENV === "production"
+    ? `3d91471f2966bcd6e6c3`
+    : `d46d572a8e9fe53f6c14`;
 export const STUDENT_REGISTRATION_LINK = `https://github.com/login/oauth/authorize?scope=user:email&client_id=${GITHUB_AUTH_CLIENT_ID}&state=student`;
 export const MENTOR_REGISTRATION_LINK = `https://github.com/login/oauth/authorize?scope=user:email&client_id=${GITHUB_AUTH_CLIENT_ID}&state=mentor`;
 export const STUDENT_MANUAL =
