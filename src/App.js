4d33e40690c68fba4ee7391f6eee07e88b01e0ab
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2023-05-16 17:08:49 +0530

    feat: removed unwanted routes

--- src/App.js
+++ src/App.js
@@ -3,21 +3,12 @@ import ReactGA from "react-ga4";
 import { BrowserRouter, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
-import OAuth from "./components/OAuth";
 import { GA_MEASUREMENT_ID } from "./constants";
 import FAQ from "./views/FAQ";
 import Home from "./views/Home";
-import MentorDashboard from "./views/MentorDashboard";
-import MentorForm from "./views/MentorForm";
-import MentorStats from "./views/MentorStats";
-import ProjectEditForm from "./views/ProjectEditFom";
-import ProjectForm from "./views/ProjectForm";
 import Projects from "./views/Projects";
 import ProjectsTable from "./views/ProjectsTable";
-import StudentDashboard from "./views/StudentDashboard";
-import StudentForm from "./views/StudentForm";
 import StudentsTable from "./views/StudentsTable";
-import StudentStats from "./views/StudentStats";
 import Testimonial from "./views/Testimonials";
 
 const ScrollToTop = () => {
@@ -51,47 +42,6 @@ function App() {
           component={Testimonial}
         />
 
-        <Route
-          onChange={ScrollToTop}
-          exact
-          path="/form/mentor"
-          component={MentorForm}
-        />
-
-        <Route
-          onChange={ScrollToTop}
-          exact
-          path="/form/student"
-          component={StudentForm}
-        />
-
-        <Route
-          onChange={ScrollToTop}
-          exact
-          path="/form/project"
-          component={ProjectForm}
-        />
-
-        <Route
-          onChange={ScrollToTop}
-          exact
-          path="/form/projectedit/:id"
-          component={ProjectEditForm}
-        />
-        {/* Dashboard of Student and Mentor */}
-        <Route
-          onChange={ScrollToTop}
-          exact
-          path="/dashboard/student"
-          component={StudentDashboard}
-        />
-
-        <Route
-          onChange={ScrollToTop}
-          exact
-          path="/dashboard/mentor"
-          component={MentorDashboard}
-        />
         {/* Stats Table for Students and Mentors */}
         <Route
           onChange={ScrollToTop}
@@ -105,22 +55,6 @@ function App() {
           path="/stats/projects"
           component={ProjectsTable}
         />
-
-        {/* Individual stats for Student and Mentor */}
-        <Route
-          onChange={ScrollToTop}
-          exact
-          path="/stats/student/:id"
-          component={StudentStats}
-        />
-        <Route
-          onChange={ScrollToTop}
-          exact
-          path="/stats/mentor/:id"
-          component={MentorStats}
-        />
-
-        <Route onChange={ScrollToTop} path="/oauth" component={OAuth} />
 
         <Route onChange={ScrollToTop} path="/projects" component={Projects} />
       </div>
