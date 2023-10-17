8419e42a8c6ac12bdc3a81e697eeb9c30a38cf7a
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2023-10-17 22:56:59 +0530

    feat: added tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
   theme: {
-    clipPath: {
-      polygonDesign:
-        "polygon(50% 0%, 80% 100px, 100% 0, 100% 100%, 80% 100%, 48% 100%, 24% 100%, 0 100%, 0 0, 21% 80px)",
-      polygonDesignSmall:
-        "polygon(50% 0%, 80% 50px, 100% 0, 100% 100%, 80% 100%, 48% 100%, 24% 100%, 0 100%, 0 0, 21% 40px)",
-    },
     extend: {},
   },
-  plugins: [require("tailwind-clip-path")],
-};
+  plugins: [],
+}
