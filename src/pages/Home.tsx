f327ccf4fe5635c83b2fbc4e5eee1c4743706679
SachdevJai <jaisachdev@kgpian.iitkgp.ac.in>
2023-10-19 16:50:03 +0530

    particles cover fullscreen now, updated font colours

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,11 +5,11 @@ import Particle from "../components/Particle";
 function Home() {
   return (
     <>
-      <section className="w-full h-screen">
+      <section>
         <Hero />
         <Particle />
       </section>
-      <section className="w-full h-screen">
+      <section className="sticky w-full h-screen z-10">
         <About />
       </section>
     </>
