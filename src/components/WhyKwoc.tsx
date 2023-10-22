d78058d42e7d4fd9288f12182ce2011b45a4d43b
SWARAJ-42 <marslegends2004@gmail.com>
2023-10-22 22:03:57 +0530

    particle system changed

--- src/components/WhyKwoc.tsx
+++ src/components/WhyKwoc.tsx
@@ -4,10 +4,12 @@ export default function WhyKwoc(){
     return(
       <>
         <h1 className="text-zinc-300 text-center mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl mt-36 lg:text-8xl">WHY KWoC</h1>
-        <p className="text-zinc-400 text-center mb-4 text-lg font-normal lg:text-2xl sm:px-16 xl:px-48 mt-10">Kharagpur Winter of Code is a 5-week long online program for students who are new to open source software development. The program not only helps students to get involved in open source, but also prepares them for many open source summer programs; Google Summer of Code being one of them.</p>
-        <div className='grid grid-cols-3 px-12'>
+        <p className="text-zinc-400 text-center mb-4 text-lg font-normal lg:text-2xl sm:px-16 xl:px-96 xl:pb-32 mt-10">Kharagpur Winter of Code is a 5-week long online program for students who are new to open source software development. The program not only helps students to get involved in open source, but also prepares them for many open source summer programs; Google Summer of Code being one of them.</p>
+        <div className='flex items-stretch flex-wrap px-8'>
             <Card header="Intro to Open Source" content="KWoC provides a great opportunity to get acquainted with Github along with Git commands and contribute to open source efficiently. Brush up your coding skills" secondary="If you love coding and want to learn about software development then KWoC helps you to get a glimpse of it and gives you a head start."/>
+
             <Card header="Prepare for GSoC" content="With KWoC, you get to know about how to select a project, interact with mentors and learn all other things that prepare you in the best way for the next GSoC."/>
+            
             <Card header="Exciting goodies" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."/>
         </div>
       </>
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,37 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./main.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import Snowfall from 'react-snowfall'
+import snowflake_particle from "./assets/snowflake.png"
+import './main.css'
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const snowflake = document.createElement('img')
+snowflake.src = snowflake_particle
+
+const images = [snowflake]
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <div>
+
+    </div>
+    <Snowfall
+      style={{
+        position: 'fixed',
+        width: '100vw',
+        minHeight: '100vh',
+        zIndex:-1
+      }}
+      // Controls the number of snowflakes that are created (defaults to 150).
+      snowflakeCount={100}
+      images={images}
+      radius={[0.5, 30.0]}
+      speed={[0.5, 6.0]}
+      wind={[-0.5, 1.0]}
+      rotationSpeed={[-1.0, 2.0]}
+    />,
+    document.querySelector('#app'),
   </React.StrictMode>,
 );
