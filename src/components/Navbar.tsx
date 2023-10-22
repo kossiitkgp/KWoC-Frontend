2a820713092b51d3b70568f83d7e7242f813b3c9
Nafisa <parvinnafisa21@gmail.com>
2023-10-22 19:54:01 +0530

    modified navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import kwoc_logo from '../assets/kwoc_logo.png'
+
 const Nav = () => {
     let Links =[
         {name:"HOME",link:"/"},
@@ -10,24 +11,23 @@ const Nav = () => {
     ];
     return (
         <>
-        <div className='shadow-md w-full fixed to-0 left-0'>
-            <div className='flex items-center justify-between bg-white py-1 px-7'>
-            <div className='font-bold text-2xl cursor-pointor flex items-center font-[Poppins] text-gray-800'>
-                <span className='text-3xl text-zinc-950 mr-1'>
+        <div className='fixed inset-x-0 top-4 z-50 w-full'>
+            <div className='flex shadow-md items-center justify-between max-w-7xl gap-4 mx-auto border border-[#FFFFFF]/[0.16] px-4 py-2 rounded-lg w-[95%] inset-x-0 backdrop-blur-md z-50'>
+            <div className='font-bold  text-2xl cursor-pointor flex items-center font-Poppins text-white'>
+                <span className='mr-1'>
                     <img className='h-10 w-10' src={kwoc_logo} alt='kwoc-logo'></img>
                 </span>
-                KWoC
-                </div>
+                KHARAGPUR WINTER OF CODE
+            </div>
             <ul className='flex flex-row items-center'>
                 {
                     Links.map((Link)=>(
-                        <li key={Link.name} className='ml-8 text-xl'>
-                            <a href={Link.link} className='text-gray-800 hover:text-gray-400 duration-500'>{Link.name}</a>
+                        <li key={Link.name} className='ml-8'>
+                            <a href={Link.link} className='font-Poppins text-white hover:text-gray-400 duration-500'>{Link.name}</a>
                         </li>
                     ))
                 }
             </ul>
-            
             </div>
         </div>
         </>
