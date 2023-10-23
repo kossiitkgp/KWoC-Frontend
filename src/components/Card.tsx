7d0206d6428e10f53cc7cd0e4432e991e3b9a26c
SWARAJ-42 <marslegends2004@gmail.com>
2023-10-23 07:45:37 +0530

    added hero-hover and card-hover animation

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,53 @@
-export default function Card(props: any){
+import { useState, MouseEvent, useCallback } from 'react';
+
+
+export default function Card(props: any) {
+
+    function throttle<T extends (...args: any[]) => any>(
+        func: T,
+        delay: number
+    ): (...args: Parameters<T>) => void {
+        let lastCall = 0;
+        return (...args: Parameters<T>) => {
+            const now = new Date().getTime();
+            if (now - lastCall < delay) {
+                return;
+            }
+            lastCall = now;
+            return func(...args);
+        };
+    }
+
+    const [rotate, setRotate] = useState({ x: 0, y: 0 });
+
+    const onMouseMove = useCallback(
+        throttle((e: MouseEvent<HTMLDivElement>) => {
+            const card = e.currentTarget;
+            const box = card.getBoundingClientRect();
+            const x = e.clientX - box.left;
+            const y = e.clientY - box.top;
+            const centerX = box.width / 2;
+            const centerY = box.height / 2;
+            const rotateX = (y - centerY) / 7;
+            const rotateY = (centerX - x) / 7;
+
+            setRotate({ x: rotateX, y: rotateY });
+        }, 100),
+        []
+    );
+
+    const onMouseLeave = () => {
+        setRotate({ x: 0, y: 0 });
+    };
+
     return (
-        <div className="backdrop-blur-sm bg-white/10 mx-auto mb-10 lg:w-1/4 md:w-full sm:w-full p-8 rounded-[20px]">
+        <div className="backdrop-blur-sm bg-white/10 mx-auto mb-10 lg:w-1/4 md:w-full sm:w-full p-8 rounded-[20px]"
+            onMouseMove={onMouseMove}
+            onMouseLeave={onMouseLeave} style={{
+            transform: `perspective(1000px) rotateX(${rotate.x / 3}deg) rotateY(${rotate.y / 3}deg) scale3d(1, 1, 1)`,
+            transition: "all 400ms cubic-bezier(0.03, 0.98, 0.52, 0.99) 0s",
+            }}
+        >
             <h1 className="text-zinc-300 text-center mb-4 font-extrabold leading-none tracking-tight text-5xl mt-16 sm:px-16 xl:px-10">{props.header}</h1>
             <p className="text-zinc-400 text-center text-base font-normal sm:px-16 xl:px-10 mt-10">{props.content}</p>
             <p className="text-zinc-400 text-center text-base font-normal sm:px-16 xl:px-10 mt-10">{props.secondary}</p>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, MouseEvent, useCallback } from 'react';
 
-function ManualButton(){
+function ManualButton() {
   return (
     <div className="flex justify-center">
       <button className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Student Manual</button>
@@ -10,7 +10,7 @@ function ManualButton(){
 }
 
 
-function Hero () {  
+function Hero() {
   const [text, setText] = useState("1000101");
 
   useEffect(() => {
@@ -21,27 +21,74 @@ function Hero () {
 
       setInterval(() => {
         setText(prevText => {
-          return prevText.split("").map((_ , index) => {
-            if (index < iteration ) {
+          return prevText.split("").map((_, index) => {
+            if (index < iteration) {
               return finalText[index];
             } else {
               return letters[Math.floor(Math.random() * (letters.length - 1))];
             }
-            }).join("");
-          })
-          iteration += 1/3;
-        }, 35);
+          }).join("");
+        })
+        iteration += 1 / 3;
+      }, 35);
+    }
+    hypr();
+  }, []);
+
+
+  function throttle<T extends (...args: any[]) => any>(
+    func: T,
+    delay: number
+  ): (...args: Parameters<T>) => void {
+    let lastCall = 0;
+    return (...args: Parameters<T>) => {
+      const now = new Date().getTime();
+      if (now - lastCall < delay) {
+        return;
       }
-      hypr();      
-    },[]);  
+      lastCall = now;
+      return func(...args);
+    };
+  }
+
+  const [rotate, setRotate] = useState({ x: 0, y: 0 });
+
+  const onMouseMove = useCallback(
+    throttle((e: MouseEvent<HTMLDivElement>) => {
+      const card = e.currentTarget;
+      const box = card.getBoundingClientRect();
+      const x = e.clientX - box.left;
+      const y = e.clientY - box.top;
+      const centerX = box.width / 2;
+      const centerY = box.height / 2;
+      const rotateX = (y - centerY) / 7;
+      const rotateY = (centerX - x) / 7;
+
+      setRotate({ x: rotateX, y: rotateY });
+    }, 100),
+    []
+  );
+
+  const onMouseLeave = () => {
+    setRotate({ x: 0, y: 0 });
+  };
 
   return (
     <div className='flex h-screen'>
       <div className='m-auto'>
-        <h1 data-value="KWoC 23" className="justify-center text-center mb-4 font-extrabold leading-none tracking-tight text-gray-900 text-[80px] lg:text-[156px] dark:text-white">
+        <h1 data-value="KWoC 23" className="justify-center text-center mb-4 font-extrabold leading-none tracking-tight text-gray-900 text-[80px] lg:text-[156px] dark:text-white"
+          onMouseMove={onMouseMove}
+          onMouseLeave={onMouseLeave} style={{
+            transform: `perspective(1000px) rotateX(${rotate.x / 3}deg) rotateY(${rotate.y / 3}deg) scale3d(1, 1, 1)`,
+            transition: "all 400ms cubic-bezier(0.03, 0.98, 0.52, 0.99) 0s",
+          }}
+        >
           {text}
         </h1>
-        <ManualButton /> 
+        <h3 className='justify-center text-center mb-16 font-extrabold leading-none tracking-tight text-gray-900 text-[20px] lg:text-[35px] dark:text-white'>
+          Kharagpur Winter of Code - 2023
+        </h3>
+        <ManualButton />
         <a href="#" className='m-auto flex justify-center mt-10'>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5} stroke="LightBlue" className="w-12 h-12 animate-bounce">
             <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 5.25l-7.5 7.5-7.5-7.5m15 6l-7.5 7.5-7.5-7.5" />
