1d1ca2e040fc75210a9cef31f22a7a4a93909f84
SWARAJ-42 <marslegends2004@gmail.com>
2023-10-23 18:44:57 +0530

    improved hero-section with animation

--- src/main.tsx
+++ src/main.tsx
@@ -3,19 +3,27 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import Snowfall from 'react-snowfall'
 import snowflake_particle from "./assets/snowflake.png"
+import snowflake_particle2 from "./assets/snowflake2.svg"
+import snowflake_particle3 from "./assets/snowflake3.svg"
+import snowflake_particle4 from "./assets/snowflake4.svg"
 import './main.css'
 
 const snowflake = document.createElement('img')
 snowflake.src = snowflake_particle
+const snowflake2 = document.createElement('img')
+snowflake2.src = snowflake_particle2
+const snowflake3 = document.createElement('img')
+snowflake3.src = snowflake_particle3
+const snowflake4 = document.createElement('img')
+snowflake4.src = snowflake_particle4
 
-const images = [snowflake]
+const images = [snowflake, snowflake2, snowflake3, snowflake4]
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
     <div>
-
     </div>
     <Snowfall
       style={{
@@ -24,13 +32,12 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         minHeight: '100vh',
         zIndex:-1
       }}
-      // Controls the number of snowflakes that are created (defaults to 150).
-      snowflakeCount={100}
+      snowflakeCount={150}
       images={images}
-      radius={[0.5, 30.0]}
-      speed={[0.5, 6.0]}
+      radius={[15.0, 40.0]}
+      speed={[0.5, 4.0]}
       wind={[-0.5, 1.0]}
-      rotationSpeed={[-1.0, 2.0]}
+      rotationSpeed={[-1.0, 1.0]}
     />,
   </React.StrictMode>,
 )
