c85c4d6feea61c24afba7fae06852e8467264299
SWARAJ-42 <marslegends2004@gmail.com>
2023-10-24 19:51:58 +0530

    fixed background styling

--- src/main.tsx
+++ src/main.tsx
@@ -1,41 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import Snowfall from 'react-snowfall'
-import snowflake_particle from "./assets/snowflake.png"
-import snowflake_particle2 from "./assets/snowflake2.svg"
-import snowflake_particle3 from "./assets/snowflake3.svg"
-import snowflake_particle4 from "./assets/snowflake4.svg"
 import './main.css'
-
-const snowflake = document.createElement('img')
-snowflake.src = snowflake_particle
-const snowflake2 = document.createElement('img')
-snowflake2.src = snowflake_particle2
-const snowflake3 = document.createElement('img')
-snowflake3.src = snowflake_particle3
-const snowflake4 = document.createElement('img')
-snowflake4.src = snowflake_particle4
-
-const images = [snowflake, snowflake2, snowflake3, snowflake4]
+import FooterSection from './components/FooterSection.tsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-    <Snowfall
-      style={{
-        position: 'fixed',
-        width: '100vw',
-        minHeight: '100vh',
-        zIndex:-1
-      }}
-      snowflakeCount={150}
-      images={images}
-      radius={[15.0, 40.0]}
-      speed={[0.5, 4.0]}
-      wind={[-0.5, 1.0]}
-      rotationSpeed={[-1.0, 1.0]}
-    />,
+    <FooterSection />
   </React.StrictMode>,
 )
