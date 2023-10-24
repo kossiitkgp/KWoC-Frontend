125220b74780945312160b2f5477db15ef19b424
SWARAJ-42 <marslegends2004@gmail.com>
2023-10-24 10:25:02 +0530

    added polygon-fencing the about-section

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
+    clipPath:{
+      polygonDesign: "polygon(50% 0%, 80% 6%, 100% 0, 100% 100%, 80% 96%, 48% 100%, 24% 94%, 0 100%, 0 0, 21% 6%)",
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('tailwind-clip-path'),
+  ],
 }
