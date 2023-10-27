efe8c80c95f5ea4513e9dcceef9d9c7234e2ed1a
Nafisa <parvinnafisa21@gmail.com>
2023-10-27 22:50:36 +0530

    removed bg img

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,10 @@ import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Testimonials from './pages/Testimonials';
 import Navbar from './components/Navbar'
-import Image from './assets/image.png'
 
 function App() {
   return (
-    <div className="App">
-      <img className='w-full h-screen opacity-80' src={Image}></img>
-      
+    <div className="App">      
       <BrowserRouter>
       <Navbar />
         <Routes>
