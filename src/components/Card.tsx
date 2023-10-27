48fe6c012ff6efe8110795d5d16c65faf7a447e2
SWARAJ-42 <marslegends2004@gmail.com>
2023-10-27 13:56:08 +0530

    removed line clamping

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -41,16 +41,18 @@ export default function Card(props: any) {
     };
 
     return (
-        <div className="text-zinc-100 text-center backdrop-blur-sm bg-white/10 mx-auto mb-10 lg:w-1/4 md:w-full sm:w-full p-8 rounded-[20px]"
+        <div className="text-zinc-100 text-center backdrop-blur-sm bg-white/10 mx-auto mb-10 lg:w-1/4 md:w-2/3 sm:w-3/4 p-8 rounded-[20px] hover:cursor-pointer"
             onMouseMove={onMouseMove}
             onMouseLeave={onMouseLeave} style={{
-            transform: `perspective(1000px) rotateX(${rotate.x / 3}deg) rotateY(${rotate.y / 3}deg) scale3d(1, 1, 1)`,
-            transition: "all 400ms cubic-bezier(0.03, 0.98, 0.52, 0.99) 0s",
+                transform: `perspective(1000px) rotateX(${rotate.x / 3}deg) rotateY(${rotate.y / 3}deg) scale3d(1, 1, 1)`,
+                transition: "all 400ms cubic-bezier(0.03, 0.98, 0.52, 0.99) 0s",
             }}
         >
             <h1 className="mb-4 font-extrabold leading-none tracking-tight text-5xl mt-16 sm:px-16 xl:px-10">{props.header}</h1>
-            <p className="text-base font-extrabold line-clamp-6 sm:px-16 xl:px-10 mt-10">{props.content}</p>
-            <p className="text-base font-extrabold line-clamp-6 sm:px-16 xl:px-10 mt-10">{props.secondary}</p>
+            <div>
+                <p className="text-base font-extrabold sm:px-16 xl:px-10 mt-10">{props.content}</p>
+                <p className="text-base font-extrabold sm:px-16 xl:px-10 mt-10">{props.secondary}</p>
+            </div>
         </div>
     );
 }
