3da453c4091a87f31d335ba984ae065780dc7b6d
SWARAJ-42 <marslegends2004@gmail.com>
2023-10-28 22:35:47 +0530

    improved responsive design, template testimonial section added

--- src/components/TestimonialCard.tsx
+++ src/components/TestimonialCard.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 interface Testimonial {
   name: string;
   role: string;
--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import TestimonialCard from './TestimonialCard';
