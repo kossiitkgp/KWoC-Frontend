038029c515cdb70a854417d829ce3ef0d54c6316
jaimin25 <jaiminchovatia25123@gmail.com>
2023-10-29 20:46:50 +0530

    made requested changes

--- src/components/JoinOurCommunity.tsx
+++ src/components/JoinOurCommunity.tsx
@@ -4,21 +4,22 @@ import SlackLogo from "../assets/slack_logo.png";
 export default function JoinOurCommunity() {
     return (
         <>
-            <h1 className="text-zinc-300 text-center mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl mt-36 lg:text-8xl">
-                Join Us
+            <h1 className="text-zinc-300 text-center mb-4 mx-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl mt-36 lg:text-8xl">
+                Join Our Community
             </h1>
-            <p className="text-zinc-300 text-center text-lg font-normal lg:text-2xl px-10 sm:px-16 xl:px-96 mt-10">
-                Community Description
-            </p>
 
-            <div className="flex flex-col md:flex-row p-4 justify-center items-center">
-                <div className="flex flex-col w-5/6 md:w-1/2 lg:w-1/3 justify-center items-center m-4 p-8 gap-6 bg-white/10 rounded-lg shadow-sm">
-                    <img
-                        src={SlackLogo}
-                        height={100}
-                        width={250}
-                    />
-
+            <div className="flex flex-col lg:flex-row p-4 justify-center items-center">
+                <div className="flex flex-col w-full md:w-4/6 lg:w-2/5 justify-center items-center m-4 p-4 py-8 gap-6 bg-black/50 backdrop-blur-lg border border-[#FFFFFF]/[0.16] rounded-lg shadow-sm">
+                    <div className="flex items-center text-center gap-2">
+                        <img
+                            src={SlackLogo}
+                            height={100}
+                            width={100}
+                        />
+                        <span className="text-white font-bold text-2xl">
+                            Slack Workspace
+                        </span>
+                    </div>
                     <a
                         className="text-white px-4 py-2 border border-white rounded hover:bg-white/25 hover:transition cursor-pointer"
                         href="https://join.slack.com/t/kwoc-koss/shared_invite/zt-wlftnk75-VoQHEEB9WpkHfza6~GGpWQ"
@@ -26,12 +27,17 @@ export default function JoinOurCommunity() {
                         Join Now
                     </a>
                 </div>
-                <div className="flex flex-col w-5/6 md:w-1/2 lg:w-1/3 justify-center items-center m-4 p-8 gap-6 bg-white/10 rounded-lg shadow-sm">
-                    <img
-                        src={DiscordLogo}
-                        height={100}
-                        width={250}
-                    />
+                <div className="flex flex-col w-full md:w-4/6 lg:w-2/5 justify-center items-center m-4 p-4 py-8 gap-6 bg-black/50 backdrop-blur-lg border border-[#FFFFFF]/[0.16] rounded-lg shadow-sm">
+                    <div className="flex items-center text-center gap-3">
+                        <img
+                            src={DiscordLogo}
+                            height={100}
+                            width={100}
+                        />
+                        <span className="text-white font-bold text-2xl">
+                            Discord Server
+                        </span>
+                    </div>
                     <a
                         className="text-white px-4 py-2 border border-white rounded hover:bg-white/25 hover:transition cursor-pointer"
                         href="https://discord.gg/efFwh6fnjk"
