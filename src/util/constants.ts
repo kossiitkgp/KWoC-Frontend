d7f1346c24521cd79491c39e839db70faafc37fb
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2023-10-30 22:02:58 +0530

    refactor: added invite links to constants

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,1 +1,3 @@
 export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+export const DISCORD_INVITE = "https://discord.gg/efFwh6fnjk";
+export const SLACK_INVITE = "https://join.slack.com/t/kwoc-koss/shared_invite/zt-wlftnk75-VoQHEEB9WpkHfza6~GGpWQ";
