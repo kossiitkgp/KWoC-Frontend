60e988089fa73310971324d944369bae187c26cb
jaimin25 <jaiminchovatia25123@gmail.com>
2023-10-30 15:22:38 +0530

    resolve conflicts

--- src/components/FooterSection.tsx
+++ src/components/FooterSection.tsx
@@ -45,49 +45,49 @@ export default function FooterSection() {
           </ul>
         </div>
 
-        <div className="w-1/2 sm:w-1/5 text-center text-zinc-300 mb-4 text-base font-extrabold">
-          <h3 className="text-2xl mb-3">Quick Links</h3>
-          <ul>
-            <li className="hover:text-white/50">
-              <a href="/#tline">Timeline</a>
-            </li>
-            <li className="hover:text-white/50">
-              <a href="/FAQ">FAQ</a>
-            </li>
-            <li className="hover:text-white/50">
-              <a href="/#about">About KWoC</a>
-            </li>
-          </ul>
-        </div>
+                <div className="w-1/2 sm:w-1/5 text-center text-zinc-300 mb-4 text-base font-extrabold">
+                    <h3 className="text-2xl mb-3">Quick Links</h3>
+                    <ul>
+                        <li className="hover:text-white/50">
+                            <a href="/#tline">Timeline</a>
+                        </li>
+                        <li className="hover:text-white/50">
+                            <a href="/FAQ">FAQ</a>
+                        </li>
+                        <li className="hover:text-white/50">
+                            <a href="/#about">About KWoC</a>
+                        </li>
+                    </ul>
+                </div>
 
-        <div className="w-1/2 sm:w-1/5 text-center text-zinc-300 mb-4 text-base font-extrabold">
-          <h3 className="text-2xl mb-3">Other</h3>
-          <ul>
-            <li className="hover:text-white/50">
-              <a
-                target="_blank"
-                rel="noreferrer noopener"
-                href="https://kossiitkgp.org"
-              >
-                About KOSS
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+                <div className="w-1/2 sm:w-1/5 text-center text-zinc-300 mb-4 text-base font-extrabold">
+                    <h3 className="text-2xl mb-3">Other</h3>
+                    <ul>
+                        <li className="hover:text-white/50">
+                            <a
+                                target="_blank"
+                                rel="noreferrer noopener"
+                                href="https://kossiitkgp.org">
+                                About KOSS
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
 
-      <div>
-        <h1 className="text-center text-zinc-300 mb-4 text-2xl sm:text-4xl font-extrabold leading-none tracking-tight mt-12">Kharagpur Winter of Code</h1>
-        <h2 className="text-center text-zinc-300 mb-4 text-xl font-extrabold leading-none tracking-tight mt-4">
-          <a
-            target="_blank"
-            rel="noreferrer noopener"
-            href="https://kossiitkgp.org"
-          >
-            With &#10084; by KOSS
-          </a>
-        </h2>
-      </div>
-    </div>
-  );
+            <div>
+                <h1 className="text-center text-zinc-300 mb-4 text-2xl sm:text-4xl font-extrabold leading-none tracking-tight mt-12">
+                    Kharagpur Winter of Code
+                </h1>
+                <h2 className="text-center text-zinc-300 mb-4 text-xl font-extrabold leading-none tracking-tight mt-4">
+                    <a
+                        target="_blank"
+                        rel="noreferrer noopener"
+                        href="https://kossiitkgp.org">
+                        With &#10084; by KOSS
+                    </a>
+                </h2>
+            </div>
+        </div>
+    );
 }
