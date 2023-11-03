696f6cbd7fae3c0f999bb6096b8c479e96707c71
Nafisa <parvinnafisa21@gmail.com>
2023-11-03 10:30:02 +0530

    Added responsive hamburger menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,7 +11,7 @@ function Navbar() {
 
   const Links = [
     { name: "HOME", link: "/" },
-    { name: "MENTOR", link: "/MentorForm" },
+    // { name: "MENTOR", link: "/MentorForm" },
     { name: "PROJECTS", link: "/Projects" },
     { name: "TESTIMONIALS", link: "/Testimonials" },
     { name: "FAQs", link: "/FAQ" },
@@ -19,7 +19,7 @@ function Navbar() {
 
   return (
     <div className='fixed inset-x-0 top-4 z-50 w-full'>
-      <div className='flex shadow-md items-center justify-between max-w-7xl gap-4 mx-auto border border-[#FFFFFF]/[0.16] px-4 py-2 rounded-lg w-[95%] inset-x-0 backdrop-blur-md z-50'>
+      <div className='flex shadow-md items-center justify-between max-w-7xl gap-4 mx-auto border border-[#FFFFFF]/[0.16] px-4 py-2 rounded-lg w-[95%] inset-x-0 backdrop-blur-md z-50 xs:max-md:text-sm'>
         <div className='font-bold flex items-center font-Poppins text-white'>
           <span className='mr-1'>
             <img className='object-contain h-12 w-12'src={kwoc_logo} alt='kwoc-logo'></img>
@@ -36,7 +36,7 @@ function Navbar() {
               </Link>
             </li>
           ))}
-          <IconContext.Provider value={{size: '3em'}}>
+          <IconContext.Provider value={{size:'3em'}}>
             <Link to='' className='mr-1'>
               <CiLogin color='#3b82f6' className={`transition-transform transform ${isHovered ? 'drop-shadow-glow scale-110' : 'scale-100'}`}
                 onMouseEnter={() => setIsHovered(true)} 
