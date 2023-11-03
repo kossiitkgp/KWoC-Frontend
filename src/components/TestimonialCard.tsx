e3ab7130d4d58fce396cb3ef17139655ef763444
Syed Sadat Ali <sadat3332@gmail.com>
2023-11-04 00:20:21 +0530

    card updated

--- src/components/TestimonialCard.tsx
+++ src/components/TestimonialCard.tsx
@@ -14,45 +14,45 @@ const TestimonialCard: React.FC<TestimonialCardProps> = ({ testimonial }) => {
   const { name, role, quote, imageLink, blogLink } = testimonial;
 
   const cardStyle = {
-    width: '900px',
+    maxWidth: '900px', 
     border: '1px solid #000',
     borderRadius: '10px',
     padding: '16px',
     boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
     transition: 'transform 0.2s ease-in-out',
     display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-between',
+    flexDirection: 'column', 
   };
 
   const leftColumnStyle = {
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
-    marginRight: '20px',
+    marginRight: '0', 
   };
 
   const rightColumnStyle = {
     flex: 1,
     display: 'flex',
     flexDirection: 'column',
-    alignItems: 'center', // Center the elements in the right column
-    textAlign: 'center', // Center-align the text
+    alignItems: 'center',
+    textAlign: 'center',
   };
+
   const imageStyle = {
-    maxWidth: '150px', // Set the maximum width for the image
-    maxHeight: '150px', // Set the maximum height for the image
-    borderRadius: '50%', // Maintain rounded shape
+    maxWidth: '150px',
+    maxHeight: '150px',
+    borderRadius: '50%',
   };
 
   return (
-    <div className="m-auto p-8" style={cardStyle}>
+    <div className="m-auto p-4" style={cardStyle}>
       <div style={leftColumnStyle}>
         <img
           src={imageLink}
           alt={name}
           className="rounded-full"
-          style = {imageStyle}
+          style={imageStyle}
         />
         <h3 className="text-zinc-400 text-2xl my-2">{name}</h3>
         <p className="text-zinc-300 text-sm mb-4">{role}</p>
@@ -61,7 +61,6 @@ const TestimonialCard: React.FC<TestimonialCardProps> = ({ testimonial }) => {
         <p className="text-zinc-300 text-xl my-2">{quote}</p>
         <a href={blogLink} target="_blank" rel="noopener noreferrer">
           <div className="px-4 py-2 bg-white/20 rounded-lg hover:bg-white/10 hover:text-black hover:shadow-md transition-all duration-300 ease-in-out">
-            
             <p className="text-white text-center font-bold">Blog</p>
           </div>
         </a>
