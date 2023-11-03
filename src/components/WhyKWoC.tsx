fb51a4a758c10b071838515a4b45b3a5de865ab9
koss-service <admin@kossiitkgp.org>
2023-11-03 21:45:48 +0000

    Applied prettier formatting

--- src/components/WhyKWoC.tsx
+++ src/components/WhyKWoC.tsx
@@ -8,18 +8,49 @@ export default function WhyKWoC() {
       </h1>
       <div className="flex flex-col lg:flex-row items-stretch px-8 m-auto mt-10 w-[90%]">
         <Card
-          header={<span>Intro to<br/>Open Source</span>}
+          header={
+            <span>
+              Intro to
+              <br />
+              Open Source
+            </span>
+          }
           content="Get started with open source collaboration. Hone programmer skills with Git and GitHub. Make meaningful contributions guided by mentors. Build a portfolio showcasing your talents."
         />
 
         <Card
-          header={<span>Prepare for<br/>GSoC</span>}
+          header={
+            <span>
+              Prepare for
+              <br />
+              GSoC
+            </span>
+          }
           content="Research organizations and connect with mentors. Create quality proposals and sharpen skills. Gain insider tips to boost your GSoC chances. Open doors through impressive abilities."
         />
 
         <Card
-          header={<span>Win Exciting<br/>Goodies</span>}
-          content={<span>Top performers in KWoC also have the opportunity to win fantastic prizes and rewards from <a className="text-blue-500 font-semibold hover:text-blue-600" href="https://fossunited.org" target="_blank">FOSS United</a>, making their participation even more rewarding.</span>}
+          header={
+            <span>
+              Win Exciting
+              <br />
+              Goodies
+            </span>
+          }
+          content={
+            <span>
+              Top performers in KWoC also have the opportunity to win fantastic
+              prizes and rewards from{" "}
+              <a
+                className="text-blue-500 font-semibold hover:text-blue-600"
+                href="https://fossunited.org"
+                target="_blank"
+              >
+                FOSS United
+              </a>
+              , making their participation even more rewarding.
+            </span>
+          }
         />
       </div>
     </>
