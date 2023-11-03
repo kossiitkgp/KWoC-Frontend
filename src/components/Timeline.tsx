c44190d17b82b852f217d15f8b8d8021f233667d
SachdevJai <jaisachdev@kgpian.iitkgp.ac.in>
2023-11-04 02:09:21 +0530

    updated timeline with final dates and font

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -28,12 +28,13 @@ function Timeline() {
         transformOrigin: 'center', 
         ease: "elastic(1.5, 1)"
       }})
-      .to(".ball02", {}, 0.08) 
+      .to(".ball02", {}, 0.10) 
       .to(".ball03", {}, 0.25)
-      .to(".ball04", {}, 0.40)
-      .to(".ball05", {}, 0.58)
-      .to(".ball06", {}, 0.75)
-      .to(".ball07", {}, 0.9)
+      .to(".ball04", {}, 0.38)
+      .to(".ball05", {}, 0.53)
+      .to(".ball06", {}, 0.67)
+      .to(".ball07", {}, 0.81)
+      .to(".ball08", {}, 0.93)
         
     const main = gsap.timeline({defaults: {duration: 1},
       scrollTrigger: {
@@ -53,52 +54,44 @@ function Timeline() {
 
   return (
     <div className=" mb-60">
-      <h1 className="text-zinc-300 text-center mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl mt-36 lg:text-8xl lg:mb-36">TIMELINE</h1>
+      <h1 className="text-zinc-300 text-center  mb-24 text-4xl font-extrabold leading-none tracking-tight md:text-5xl mt-36 lg:text-8xl lg:mb-36">TIMELINE</h1>
       <div className="flex justify-center mb-1/10">
-        <svg className="md:scale-110 lg:scale-125 mt-36" id="svg" width="900" height="1579" viewBox="0 0 403 1579" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <g id="Frame 1">
+        <svg id="svg" width="410" height="1760" viewBox="0 0 392 1760" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M36.9996 35C340.333 188.333 403.5 260.5 286.5 334C115.03 441.718 56.9995 485.5 56.9995 518.5C56.9995 639 608 716.5 267 842C171 877.331 -74.3463 984.362 160 1107.5C435 1252 493.162 1227.01 137 1412C-53 1510.69 110 1576 380.5 1734" stroke="#808080" stroke-width="10" stroke-linecap="round"/>
+        <path className="theLine" d="M36.9996 35C340.333 188.333 403.5 260.5 286.5 334C115.03 441.718 56.9995 485.5 56.9995 518.5C56.9995 639 608 716.5 267 842C171 877.331 -74.3463 984.362 160 1107.5C435 1252 493.162 1227.01 137 1412C-53 1510.69 110 1576 380.5 1734" stroke="white" stroke-width="10" stroke-linecap="round"/>
 
-            <path d="M9.99976 30C313.333 183.333 416.5 341.5 286.5 391.5C97.4998 464.192 63.4998 480.5 63.4998 513.5C63.4998 634 516.526 745.129 358.5 808C265.5 845 -46.4881 953.224 112.5 1047C260 1134 662 1227.5 73 1486" stroke="gray" stroke-width="10" stroke-linecap="round"/>
-            <path className="theLine" d="M9.99976 30C313.333 183.333 416.5 341.5 286.5 391.5C97.4998 464.192 63.4998 480.5 63.4998 513.5C63.4998 634 516.526 745.129 358.5 808C265.5 845 -46.4881 953.224 112.5 1047C260 1134 662 1227.5 73 1486" stroke="white" stroke-width="10" stroke-linecap="round"/>
-
-            <path className="Line-1" d="M10 29.5H388" stroke="gray" stroke-width="2"/>
-            <path className="Line-2" d="M10 273H324" stroke="gray" stroke-width="2"/>
-            <path className="Line-3" d="M65 516L388 516" stroke="gray" stroke-width="2"/>
-            <path className="Line-4" d="M65 759L388 759" stroke="gray" stroke-width="2"/>
-            <path className="Line-5" d="M65 1002L388 1002" stroke="gray" stroke-width="2"/>
-            <path className="Line-6" d="M65 1245L388 1245" stroke="gray" stroke-width="2"/>
-            <path className="Line-7" d="M71 1488L394 1488" stroke="gray" stroke-width="2"/>
-            
-            <circle className="ball01" cx="13" cy="30" r="16" fill="white"/>
-            <circle visibility={'hidden'} className="ball02" cx="326" cy="273.5" r="8" fill="gray"/>
-            <circle visibility={'hidden'} className="ball03" cx="65" cy="517" r="8" fill="gray"/>
-            <circle visibility={'hidden'} className="ball04" cx="384" cy="760" r="8" fill="gray"/>
-            <circle visibility={'hidden'} className="ball05" cx="70" cy="1003" r="8" fill="gray"/>
-            <circle visibility={'hidden'} className="ball06" cx="383" cy="1246" r="8" fill="gray"/>
-            <circle visibility={'hidden'} className="ball07" cx="71" cy="1489" r="8" fill="gray"/>
-            
-            <text className="text1" fill="white" font-family="Space Grotesk" font-size="25" letter-spacing="0em"><tspan x="162.878" y="24.65">25 November 2023</tspan></text>
-            <text className="subtext1" fill="white" font-family="Space Grotesk" font-size="15" letter-spacing="0em"><tspan x="180.85" y="47.69">Registrations for Students </tspan><tspan x="209.399" y="66.69">and Projects Begin</tspan></text>
-            
-            <text className="text5" fill="white" font-family="Space Grotesk" font-size="25" letter-spacing="0em"><tspan x="161.021" y="995.65">20 December 2023</tspan></text>
-            <text className="subtext5" fill="white" font-family="Space Grotesk" font-size="15" letter-spacing="0em"><tspan x="221.972" y="1018.69">Mid Evaluations</tspan></text>
-            
-            <text className="text3" fill="white" font-family="Space Grotesk" font-size="25" letter-spacing="0em"><tspan x="166.588" y="510.65">12 December 2023</tspan></text>
-            <text className="subtext3" fill="white" font-family="Space Grotesk" font-size="15" letter-spacing="0em"><tspan x="180.919" y="533.69">End of Project Registration</tspan></text>
-            
-            <text className="text2" fill="white" font-family="Space Grotesk" font-size="25" letter-spacing="0em"><tspan x="10" y="267.65">6 December 2023</tspan></text>
-            <text className="subtext2" fill="white" font-family="Space Grotesk" font-size="15" letter-spacing="0em"><tspan x="45.6025" y="287.69">Coding Period Starts</tspan></text>
-            
-            <text className="text4" fill="white" font-family="Space Grotesk" font-size="25" letter-spacing="0em"><tspan x="65" y="754.65">17 December 2023</tspan></text>
-            <text className="subtext4" fill="white" font-family="Space Grotesk" font-size="15" letter-spacing="0em"><tspan x="73.979" y="775.69">End of Student Registration</tspan></text>
-            
-            <text className="text7" fill="white" font-family="Space Grotesk" font-size="25" letter-spacing="0em"><tspan x="206.427" y="1482.65">8 January 2024</tspan></text>
-            <text className="subtext7" fill="white" font-family="Space Grotesk" font-size="15" letter-spacing="0em"><tspan x="215.698" y="1505.69">Deadline to Submit End </tspan><tspan x="254.905" y="1524.69">Evals Report </tspan></text>
-            
-            <text className="text6" fill="white" font-family="Space Grotesk" font-size="25" letter-spacing="0em"><tspan x="65" y="1237.65">4 January 2024</tspan></text>
-            <text className="subtext6" fill="white" font-family="Space Grotesk" font-size="15" letter-spacing="0em"><tspan x="87.1348" y="1263.69">Coding Period Ends 
-            </tspan><tspan x="67.3667" y="1282.69">but we encourage you to </tspan><tspan x="93.6826" y="1301.69">keep contributing</tspan></text>
-          </g>
+        <path d="M39 35L382 35" stroke="#808080" stroke-width="2"/>
+        <path d="M3.50012 278H338.5" stroke="#808080" stroke-width="2"/>
+        <path d="M58.5001 521H381.5" stroke="#808080" stroke-width="2"/>
+        <path d="M58.5001 764H381.5" stroke="#808080" stroke-width="2"/>
+        <path d="M58.5001 1493H381.5" stroke="#808080" stroke-width="2"/>
+        <path d="M58.5001 1736H381.5" stroke="#808080" stroke-width="2"/>
+        <path d="M58.5001 1250H381.5" stroke="#808080" stroke-width="2"/>
+        <path d="M58.5001 1007H381.5" stroke="#808080" stroke-width="2"/>
+        <text fill="white" font-family="Cantarell" font-size="25" font-weight="500" letter-spacing="0em"><tspan x="171.476" y="27.1558">8 November 2023</tspan></text>
+        <text fill="white" font-family="Roboto" font-size="15" letter-spacing="0em"><tspan x="192.193" y="56.127">Mentor Registrations Begins</tspan></text>
+        <text fill="white" font-family="Cantarell" font-size="25" font-weight="500" letter-spacing="0em"><tspan x="156" y="999.156">16 December 2023</tspan></text>
+        <text fill="white" font-family="Cantarell" font-size="15" font-weight="500" letter-spacing="0em"><tspan x="271" y="1025.29">Mid Evaluations </tspan></text>
+        <text fill="white" font-family="Cantarell" font-size="25" font-weight="500" letter-spacing="0em"><tspan x="175" y="513.156">2 December 2023</tspan></text>
+        <text fill="white" font-family="Cantarell" font-size="15" font-weight="500" letter-spacing="0em"><tspan x="271" y="538.293">Mid Evaluations </tspan></text>
+        <text fill="white" font-family="Cantarell" font-size="25" font-weight="500" letter-spacing="0em"><tspan x="0" y="266.156">15 November 2023</tspan></text>
+        <text fill="white" font-family="Cantarell" font-size="25" font-weight="500" letter-spacing="0em"><tspan x="200" y="1486.16">7 January 2024</tspan></text>
+        <text fill="white" font-family="Cantarell" font-size="15" font-weight="500" letter-spacing="0em"><tspan x="199" y="1512.29">Final Submission Deadline</tspan></text>
+        <text fill="white" font-family="Cantarell" font-size="25" font-weight="500" letter-spacing="0em"><tspan x="58" y="1242.16">31December 2023</tspan></text>
+        <text fill="white" font-family="Cantarell" font-size="15" font-weight="500" letter-spacing="0em"><tspan x="58" y="1268.29">Student and Mentor Registrations </tspan><tspan x="58" y="1289.29">Close</tspan></text>
+        <text fill="white" font-family="Cantarell" font-size="25" font-weight="500" letter-spacing="0em"><tspan x="58" y="1728.16">21 January 2024</tspan></text>
+        <text fill="white" font-family="Cantarell" font-size="15" font-weight="500" letter-spacing="0em"><tspan x="58" y="1755.29">Results</tspan></text>
+        <text fill="white" font-family="Cantarell" font-size="15" font-weight="500" letter-spacing="0em"><tspan x="4" y="298.293">Student Registration Begins </tspan></text>
+        <text fill="white" font-family="Cantarell" font-size="25" font-weight="500" letter-spacing="0em"><tspan x="56" y="756.156">9 December 2023</tspan></text>
+        <text fill="white" font-family="Cantarell" font-size="15" font-weight="500" letter-spacing="0em"><tspan x="56" y="782.293">Student and Mentor Registrations </tspan><tspan x="56" y="803.293">Close</tspan></text>
+        <circle className="ball01" cx="39.5" cy="34.5" r="22.5" fill="white"/>
+        <circle className="ball02" visibility={'hidden'} cx="339.5" cy="277.5" r="7.5" fill="#808080"/>
+        <circle className="ball03" visibility={'hidden'} cx="57.5001" cy="520.5" r="7.5" fill="#808080"/>
+        <circle className="ball04" visibility={'hidden'} cx="378.5" cy="763.5" r="7.5" fill="#808080"/>
+        <circle className="ball05" visibility={'hidden'} cx="62.5001" cy="1007.5" r="7.5" fill="#808080"/>
+        <circle className="ball06" visibility={'hidden'} cx="384.5" cy="1249.5" r="7.5" fill="#808080"/>
+        <circle className="ball07" visibility={'hidden'} cx="54.5" cy="1492.5" r="7.5" fill="#808080"/>
+        <circle className="ball08" visibility={'hidden'} cx="380.5" cy="1734.5" r="7.5" fill="#808080"/>
         </svg>
       </div>
     </div>
