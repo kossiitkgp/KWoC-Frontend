f0bea0575ea47918e70b69ec743a92557b282c76
Syed Sadat Ali <sadat3332@gmail.com>
2023-11-03 23:30:56 +0530

    card made wider

--- src/components/TestimonialCard.tsx
+++ src/components/TestimonialCard.tsx
@@ -14,30 +14,32 @@ const TestimonialCard: React.FC<TestimonialCardProps> = ({ testimonial }) => {
   const { name, role, quote, imageLink, blogLink } = testimonial;
 
   const cardStyle = {
-    border: '1px solid #000', // Set the border style here
-    borderRadius: '10px', // Add rounded corners to the card
+    border: '1px solid #000',
+    borderRadius: '10px',
     padding: '16px',
+    boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)', 
+    transition: 'transform 0.2s ease-in-out', 
   };
 
   return (
     <div className="m-auto p-8" style={cardStyle}>
-      <div className="w-1/3 sm:w-1/5 m-auto">
+      <div className="w-1/6 sm:w-1/15 m-auto">
         <img
           src={imageLink}
           alt={name}
-          className="rounded-full" // Apply rounded corners for a circular image
+          className="rounded-full"
         />
       </div>
       <h3 className="text-zinc-400 text-2xl my-2">{name}</h3>
       <p className="text-zinc-300 text-sm mb-4">{role}</p>
       <div className="sm:w-1/5 m-auto">
         <a href={blogLink} target="_blank" rel="noopener noreferrer">
-          <div className="px-4 py-2 bg-white/20 rounded-lg hover:bg-white/10">
-            <p className="text-zinc-400 text-center font-bold">Blog</p>
-          </div>
+        <div className="px-4 py-2 bg-white/20 rounded-lg hover:bg-white/10 hover:text-black hover:shadow-md transition-all duration-300 ease-in-out">
+  <p className="text-white text-center font-bold">Blog</p>
+</div>
         </a>
       </div>
-      <p className="text-zinc-400 text-1xl my-4 px-4 sm:px-16">{quote}</p>
+      <p className="text-white text-2xl my-4 px-4 sm:px-16">{quote}</p>
     </div>
   );
 };
--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -4,20 +4,16 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import TestimonialCard from './TestimonialCard';
 import testimonialsData from '../data/testimonials.json';
 
+
 export default class Testimonials extends Component {
   render() {
-    const glassStyle = {
-      backgroundColor: 'rgba(0, 0, 0, 0.5)', 
-      backdropFilter: 'blur(10px)', 
-      border: '1px solid rgba(255, 255, 255, 0.16)', 
-    };
-
     return (
-      <div className="m-auto w-[90%]" style={glassStyle}>
-        <div className="w-[90%] sm:w-[90%] md:w-[80%] lg:w-[60%] m-auto pt-16 sm:p-32">
-          <h1 className="text-zinc-300 text-center mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl lg:text-5xl">
+      <div className="testimonials-container">
+        <div className="content-container">
+          <h1 className="text-zinc-300 text-center mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl lg:text-7xl">
             What Developers think about us
           </h1>
+          <div className="my-8 md:my-20"></div>
           <Carousel
             showArrows={true}
             infiniteLoop={true}
