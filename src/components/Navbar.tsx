2820895aca0263fb9937c1e7c6f959c41d298292
Nafisa <parvinnafisa21@gmail.com>
2023-11-04 01:29:47 +0530

    aligned navbar to right

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -75,7 +75,7 @@ function Navbar() {
         });
       }
     };
-  }, []); 
+  }, []);
 
   const [menuOpen, setMenuOpen] = useState(false);
   const location = useLocation();
@@ -95,71 +95,71 @@ function Navbar() {
   };
 
   return (
-    <div className="fixed inset-x-0 top-4 z-50 w-full">
-      <nav className={`flex shadow-md items-center justify-between max-w-7xl gap-4 mx-auto border border-[#FFFFFF]/[0.16] px-4 py-2 rounded-lg w-[95%] inset-x-0 backdrop-blur-md z-50 ${menuOpen ? 'hidden' : null}`}>
+    <div className="fixed inset-x-0 z-50 w-full">
+      <nav className={`flex shadow-md mt-4 items-center justify-between max-w-7xl gap-4 mx-auto border border-[#FFFFFF]/[0.16] px-4 py-2 rounded-lg w-[95%] inset-x-0 backdrop-blur-md z-50 ${menuOpen ? 'hidden' : null}`}>
         <div className='font-bold flex items-center font-Poppins text-white'>
           <span className='mr-1'>
             <img className="object-contain h-12 w-12" src={kwoc_logo} alt='kwoc-logo'></img>
           </span>
-          </div>
+        </div>
         <div className="lg:hidden ml-auto -mr-5">
           <button className="navbar-burger flex items-center text-blue-600 p-3" onClick={toggleMenu}>
             <CiMenuBurger size='2em' color='#3b82f6' />
           </button>
         </div>
-        
+
         <div className={`${menuOpen ? 'hidden' : null}`}>
-        <ul className={`lg:flex lg:items-center lg:w-auto lg:space-x-6 ${menuOpen ? 'block' : 'hidden'}`}>
-          {Links.map((link) => (
-            <li key={link.name} className='md:ml-4 md:mr-4'>
-              <Link to={link.link} className={`px-2 py-1 ${isLinkActive(link.link) ? 'text-blue-500 hover:drop-shadow-glow duration-500' : 'text-white opacity-80 hover:drop-shadow-glow duration-500 active:text-blue-700'}`}>
-                {link.name}
+          <ul className={`lg:flex lg:items-center lg:w-auto lg:space-x-6 ${menuOpen ? 'block' : 'hidden'}`}>
+            {Links.map((link) => (
+              <li key={link.name} className='md:ml-4 md:mr-4'>
+                <Link to={link.link} className={`px-2 py-1 ${isLinkActive(link.link) ? 'text-blue-500 hover:drop-shadow-glow duration-500' : 'text-white opacity-80 hover:drop-shadow-glow duration-500 active:text-blue-700'}`}>
+                  {link.name}
+                </Link>
+              </li>
+            ))}
+            <IconContext.Provider value={{ size: '3em' }}>
+              <Link to=''>
+                <CiLogin color='#dc2626' className={`transition-transform transform ${isHovered ? 'drop-shadow-glow scale-110' : 'scale-100'}`}
+                  onMouseEnter={() => setIsHovered(true)}
+                  onMouseLeave={() => setIsHovered(false)} />
               </Link>
-            </li>
-          ))}
-          <IconContext.Provider value={{size:'3em'}}>
-            <Link to=''>
-              <CiLogin color='#3b82f6' className={`transition-transform transform ${isHovered ? 'drop-shadow-glow scale-110' : 'scale-100'}`}
-                onMouseEnter={() => setIsHovered(true)} 
-                onMouseLeave={() => setIsHovered(false)} />
-            </Link>
-          </IconContext.Provider>
-        </ul>
+            </IconContext.Provider>
+          </ul>
         </div>
       </nav>
-      <div className={`navbar-menu relative z-50 ${menuOpen ? 'block' : 'hidden'}`}>
-        <div className="navbar-backdrop fixed inset-0 bg-gray-800 opacity-25"></div>
-        <nav className="fixed top-0 left-0 bottom-0 flex flex-col w-5/6 max-w-sm py-7 px-7 border border-[#FFFFFF]/[0.16] rounded-lg inset-x-0 backdrop-blur-md overflow-y-auto">
+      {menuOpen && <div className='navbar-menu w-full z-50 h-screen transition-transform transform ease-in-out duration-500 translate-x-0 p-2 flex justify-end'>
+        <div className="navbar-backdrop fixed bg-gray-800 opacity-25"></div>
+        <nav className="h-full flex flex-col w-5/6 max-w-sm py-5 px-5 border border-[#FFFFFF]/[0.16] rounded-lg backdrop-blur-md overflow-y-auto">
           <div className="flex-items-center flex flex-row mb-8 mx-2">
             <span className="mr-auto text-3xl font-bold leading-none">
               <img className='object-contain h-12 w-12' src={kwoc_logo} alt='kwoc-logo'></img>
             </span>
             <button className="navbar-close" onClick={toggleMenu}>
-              <RiCloseLine size='1.5em' color='#3b82f6'/>
+              <RiCloseLine size='2.5em' color='#3b82f6' />
             </button>
           </div>
           <div>
-            <ul className='ml-4'>
+            <ul className='mr-4 text-right'>
               {Links.map((link) => (
-                <li key={link.name} className='mb-1'>
+                <li key={link.name} className='mb-1' onClick={toggleMenu}>
                   <Link to={link.link} className={`block p-3 text-sm font-semibold ${isLinkActive(link.link) ? 'text-blue-500 hover:drop-shadow-glow duration-500' : 'text-white opacity-80 hover:drop-shadow-glow duration-500 active:text-blue-700'}`}>
                     {link.name}
                   </Link>
                 </li>
               ))}
-              <IconContext.Provider value={{size:'3em'}}>
-                <Link to=''>
-                  <CiLogin color='#3b82f6' className={`transition-transform transform ${isHovered ? 'drop-shadow-glow scale-110' : 'scale-100'}`}
-                    onMouseEnter={() => setIsHovered(true)} 
+              <IconContext.Provider value={{ size: '2.5em' }}>
+                <Link to='' className='flex justify-end pr-2 pt-2'>
+                  <CiLogin color='#dc2626' className={`transition-transform transform ${isHovered ? 'drop-shadow-glow scale-110' : 'scale-100'}`}
+                    onMouseEnter={() => setIsHovered(true)}
                     onMouseLeave={() => setIsHovered(false)} />
                 </Link>
               </IconContext.Provider>
-                        </ul>
-                    </div>
-                </nav>
-            </div>
-        </div>
-    );
+            </ul>
+          </div>
+        </nav>
+      </div>}
+    </div>
+  );
 };
 
 
