a3ca11989682256ce3677068fd76fb6f24f9f952
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2023-11-04 02:28:16 +0530

    refactor: removed unused code

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { CiMenuBurger } from "react-icons/ci";
 import { RiCloseLine } from "react-icons/ri";
@@ -7,89 +7,20 @@ import { IconContext } from "react-icons";
 import kwoc_logo from "../assets/kwoc_logo.png";
 import { GH_OAUTH_URL, ROUTER_PATHS } from "../util/constants";
 
+const Links = [
+  { name: "HOME", link: ROUTER_PATHS.HOME },
+  { name: "MENTOR", link: ROUTER_PATHS.MENTOR_FORM },
+  { name: "PROJECTS", link: ROUTER_PATHS.PROJECTS_LIST },
+  { name: "TESTIMONIALS", link: ROUTER_PATHS.TESTIMONIALS },
+  { name: "FAQs", link: ROUTER_PATHS.FAQ },
+];
+
 function Navbar() {
-  useEffect(() => {
-    const burger = document.querySelectorAll(".navbar-burger");
-    const menu = document.querySelectorAll(".navbar-menu");
-
-    if (burger.length && menu.length) {
-      burger.forEach((item) => {
-        item.addEventListener("click", () => {
-          menu.forEach((m) => {
-            m.classList.toggle("hidden");
-          });
-        });
-      });
-    }
-
-    const close = document.querySelectorAll(".navbar-close");
-    const backdrop = document.querySelectorAll(".navbar-backdrop");
-
-    if (close.length) {
-      close.forEach((item) => {
-        item.addEventListener("click", () => {
-          menu.forEach((m) => {
-            m.classList.toggle("hidden");
-          });
-        });
-      });
-    }
-
-    if (backdrop.length) {
-      backdrop.forEach((item) => {
-        item.addEventListener("click", () => {
-          menu.forEach((m) => {
-            m.classList.toggle("hidden");
-          });
-        });
-      });
-    }
-
-    return () => {
-      if (burger.length && menu.length) {
-        burger.forEach((item) => {
-          item.removeEventListener("click", () => {
-            menu.forEach((m) => {
-              m.classList.toggle("hidden");
-            });
-          });
-        });
-      }
-
-      if (close.length) {
-        close.forEach((item) => {
-          item.removeEventListener("click", () => {
-            menu.forEach((m) => {
-              m.classList.toggle("hidden");
-            });
-          });
-        });
-      }
-
-      if (backdrop.length) {
-        backdrop.forEach((item) => {
-          item.removeEventListener("click", () => {
-            menu.forEach((m) => {
-              m.classList.toggle("hidden");
-            });
-          });
-        });
-      }
-    };
-  }, []);
-
-  const [menuOpen, setMenuOpen] = useState(false);
   const location = useLocation();
   const isLinkActive = (path: string) => location.pathname === path;
-  const [isHovered, setIsHovered] = useState(false);
 
-  const Links = [
-    { name: "HOME", link: ROUTER_PATHS.HOME },
-    { name: "MENTOR", link: ROUTER_PATHS.MENTOR_FORM },
-    { name: "PROJECTS", link: ROUTER_PATHS.PROJECTS_LIST },
-    { name: "TESTIMONIALS", link: ROUTER_PATHS.TESTIMONIALS },
-    { name: "FAQs", link: ROUTER_PATHS.FAQ },
-  ];
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
@@ -98,9 +29,7 @@ function Navbar() {
   return (
     <div className="fixed inset-x-0 z-50 w-full">
       <nav
-        className={`flex shadow-md mt-4 items-center justify-between max-w-7xl gap-4 mx-auto border border-[#FFFFFF]/[0.16] px-4 py-2 rounded-lg w-[95%] inset-x-0 backdrop-blur-md z-50 ${
-          menuOpen ? "hidden" : null
-        }`}
+        className={`flex shadow-md mt-4 items-center justify-between max-w-7xl gap-4 mx-auto border border-[#FFFFFF]/[0.16] px-4 py-2 rounded-lg w-[95%] inset-x-0 backdrop-blur-md z-50 ${menuOpen ? "hidden" : "null"}`}
       >
         <div className="font-bold flex items-center font-Poppins text-white">
           <span className="mr-1">
@@ -108,12 +37,13 @@ function Navbar() {
               className="object-contain h-12 w-12"
               src={kwoc_logo}
               alt="kwoc-logo"
-            ></img>
+            />
           </span>
         </div>
+
         <div className="lg:hidden ml-auto -mr-5">
           <button
-            className="navbar-burger flex items-center text-blue-600 p-3"
+            className="flex items-center text-blue-600 p-3"
             onClick={toggleMenu}
           >
             <CiMenuBurger size="2em" color="#3b82f6" />
@@ -122,9 +52,7 @@ function Navbar() {
 
         <div className={`${menuOpen ? "hidden" : null}`}>
           <ul
-            className={`lg:flex lg:items-center lg:w-auto lg:space-x-6 ${
-              menuOpen ? "block" : "hidden"
-            }`}
+            className={`lg:flex lg:items-center lg:w-auto lg:space-x-6 ${menuOpen ? "block" : "hidden"}`}
           >
             {Links.map((link) => (
               <li key={link.name} className="md:ml-4 md:mr-4">
@@ -140,13 +68,12 @@ function Navbar() {
                 </Link>
               </li>
             ))}
+
             <IconContext.Provider value={{ size: "3em" }}>
               <Link to={GH_OAUTH_URL}>
                 <CiLogin
                   color="#dc2626"
-                  className={`transition-transform transform ${
-                    isHovered ? "drop-shadow-glow scale-110" : "scale-100"
-                  }`}
+                  className={`transition-transform transform ${isHovered ? "drop-shadow-glow scale-110" : "scale-100"}`}
                   onMouseEnter={() => setIsHovered(true)}
                   onMouseLeave={() => setIsHovered(false)}
                 />
@@ -156,7 +83,7 @@ function Navbar() {
         </div>
       </nav>
       {menuOpen && (
-        <div className="navbar-menu w-full z-50 h-screen transition-transform transform ease-in-out duration-500 translate-x-0 p-2 flex justify-end">
+        <div className="w-full z-50 h-screen transition-transform transform ease-in-out duration-500 translate-x-0 p-2 flex justify-end">
           <div className="navbar-backdrop fixed bg-gray-800 opacity-25"></div>
           <nav className="h-full flex flex-col w-5/6 max-w-sm py-5 px-5 border border-[#FFFFFF]/[0.16] rounded-lg backdrop-blur-md overflow-y-auto">
             <div className="flex-items-center flex flex-row mb-8 mx-2">
@@ -167,7 +94,7 @@ function Navbar() {
                   alt="kwoc-logo"
                 ></img>
               </span>
-              <button className="navbar-close" onClick={toggleMenu}>
+              <button onClick={toggleMenu}>
                 <RiCloseLine size="2.5em" color="#3b82f6" />
               </button>
             </div>
