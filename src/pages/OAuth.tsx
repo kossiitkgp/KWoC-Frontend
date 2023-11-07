ab2243abe36b8cbf48ca58ff3513f6a8f05a3db6
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2023-11-07 15:16:36 +0530

    feat: handle request rejection error

--- src/pages/OAuth.tsx
+++ src/pages/OAuth.tsx
@@ -12,35 +12,41 @@ function OAuth() {
   const loginHandler = async (oauthCode: string) => {
     // Assuming type is already set when login is started
     const userType = authContext.userData.type;
-    const authRes = await makeRequest("oauth", "post", {
-      code: oauthCode,
-      type: userType,
-    });
-
-    if (!authRes.is_ok) {
-      setError(authRes.response.message);
-    } else {
-      const auth = authRes.response;
-
-      authContext.onLogin({
-        jwt: auth.jwt,
-        userData: {
-          username: auth.username,
-          name: auth.name,
-          email: auth.email,
-          type: auth.type,
-        },
+    try {
+      const authRes = await makeRequest("oauth", "post", {
+        code: oauthCode,
+        type: userType,
       });
 
-      switch (auth.type) {
-        case 'student':
-          navigate(auth.is_new_user ? ROUTER_PATHS.STUDENT_FORM : ROUTER_PATHS.STUDENT_DASHBOARD);
-          break;
+      if (!authRes.is_ok) {
+        setError(authRes.response.message);
+      } else {
+        const auth = authRes.response;
 
-        case 'mentor':
-          navigate(auth.is_new_user ? ROUTER_PATHS.MENTOR_FORM : ROUTER_PATHS.MENTOR_DASHBOARD);
-          break;
+        authContext.onLogin({
+          jwt: auth.jwt,
+          userData: {
+            username: auth.username,
+            name: auth.name,
+            email: auth.email,
+            type: auth.type,
+          },
+        });
+
+        switch (auth.type) {
+          case 'student':
+            navigate(auth.is_new_user ? ROUTER_PATHS.STUDENT_FORM : ROUTER_PATHS.STUDENT_DASHBOARD);
+            break;
+
+          case 'mentor':
+            navigate(auth.is_new_user ? ROUTER_PATHS.MENTOR_FORM : ROUTER_PATHS.MENTOR_DASHBOARD);
+            break;
+        }
       }
+    }
+    catch (e) {
+      setError('Error connecting to the server. Please try again later.');
+      console.log(e);
     }
   };
 
