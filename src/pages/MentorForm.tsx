1642d7fc21758fa3d75bcbe2d378839d7a2e1da1
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2023-11-07 17:24:25 +0530

    feat: completed mentor form fields

--- src/pages/MentorForm.tsx
+++ src/pages/MentorForm.tsx
@@ -1,44 +1,43 @@
-function MentorForm(){
+import { useEffect } from "react";
+import Form from "../components/Form";
+import { useAuthContext } from "../util/auth";
+import { ROUTER_PATHS } from "../util/constants";
+import { useNavigate } from "react-router-dom";
 
-  return(
-  <div className="w-full h-screen bg-slate-900 pt-28">
-  <div className="w-full md:w-96 md:max-w-full mx-auto rounded-md">
-    <div className="p-10 border border-slate-700 sm:rounded-md">
-      <form>
-        <h1 className="text-center text-3xl font-poppins italic text-white mb-10 ">Mentor Form</h1>
-        <label className="block mb-6">
-            <span className="text-white">Full Name</span>
-            <input
-              name="name"
-              type="name"
-              className="block w-full mt-2 px-2 py-1 bg-gray-800 text-white border-slate-700 rounded-md shadow-sm focus:border-indigo-700 focus:ring focus:ring-indigo-700 focus:ring-opacity-50"
-              placeholder="Jane Doe"
-              required
-            />
-          </label>
-          <label className="block mb-10">
-            <span className="text-white">Email address</span>
-            <input
-              name="email"
-              type="email"
-              className="block w-full mt-2 px-2 py-1 bg-gray-800 text-white border-slate-700 rounded-md shadow-sm focus:border-indigo-700 focus:ring focus:ring-indigo-700 focus:ring-opacity-50"
-              placeholder="jane.doe@example.com"
-              required
-            />
-          </label>
-          <div className="mb-4 text-center">
-            <button
-              type="submit"
-              className="h-10 px-5 text-indigo-100 bg-indigo-700 rounded-lg transition-colors duration-150 focus:shadow-outline hover:bg-indigo-800"
-            >
-              Submit
-            </button>
-          </div>
-      </form>
-    </div>
-  </div>
-  </div>
-  );
-  }
-  
-  export default MentorForm;
+function MentorForm() {
+  const authContext = useAuthContext();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!authContext.isAuthenticated) {
+      navigate(ROUTER_PATHS.HOME);
+    }
+
+    if (authContext.userData.type !== 'mentor') {
+      navigate(ROUTER_PATHS.HOME);
+    }
+  })
+
+  return <Form
+    title="Your Information"
+    fields={{
+      name: {
+        field: 'Name',
+        placeholder: 'Jane Doe',
+        type: 'text',
+        defaultValue: authContext.userData.name ?? '',
+        required: true
+      },
+      email: {
+        field: 'Email',
+        placeholder: 'jane.doe@example.com',
+        type: 'email',
+        defaultValue: authContext.userData.email ?? '',
+        required: true
+      }
+    }}
+    onSubmit={console.log}
+  />
+}
+
+export default MentorForm;
