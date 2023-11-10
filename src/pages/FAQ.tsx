d400efbfeb2f313a0e363fd4eef6d49c6aaa2ae8
Rohan Barsagade <rbarsagade101@gmail.com>
2023-11-10 11:45:02 +0530

    feat: FAQs page added

--- src/pages/FAQ.tsx
+++ src/pages/FAQ.tsx
@@ -1,5 +1,73 @@
-function FAQ() {
-  return <div className="faq">This website is a work in progress.</div>;
-}
+import Fuse from "fuse.js";
+import React, { useState } from "react";
+import FAQs from "../data/FAQs.json";
 
-export default FAQ;
+export default function FAQ() {
+  const [query, setQuery] = useState("");
+
+  const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+  };
+
+  const fuse = new Fuse(FAQs, {
+    keys: ["question"],
+  });
+
+  const results = fuse.search(query);
+
+  const searchResults = query ? results.map((result) => result.item) : FAQs;
+
+  return (
+    <div className="flex flex-col items-center pt-24">
+      <div className="sticky lg:-top-40 md:-top-40 sm:-top-52 max-[640px]:-top-[10rem] max-[540px]:-top-[12rem] max-[500px]:-top-[14rem] max-[300px]:-top-[18rem] flex flex-col items-center bg-[#0a0a19]">
+        <section className="px-16 py-4 my-8 mx-0">
+          <h1 className="font-bold text-3xl sm:text-5xl mb-8 text-center">
+            Frequently Asked Questions
+          </h1>
+          <p className="text-xl font-semibold mb-16 text-center">
+            Wanna ask us a question? Check these first.
+          </p>
+        </section>
+
+        <div className="p-4 my-4 mx-0">
+          <div className="w-full bg-none border-none">
+            <input
+              className="py-4 px-6 rounded-md outline-none w-[80vw] max-w-3xl border-none text-black font-semibold bg-none"
+              type="text"
+              placeholder="Search your query"
+              onChange={onChangeHandler}
+              value={query}
+            ></input>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col items-center max-w-3xl px-4">
+        <ul className="list-none py-2 space-y-12">
+          {searchResults.map((FAQ, i) => {
+            const { question, answer } = FAQ;
+            return (
+              <li className="space-y-4" key={`q-${i}`}>
+                <div className="font-bold text-lg">
+                  <strong>{question}</strong>
+                </div>
+                <div>
+                  <ul className="list-disc mx-12 space-y-2 leading-6">
+                    {answer.map((item) => {
+                      return (
+                        <li
+                          key={item}
+                          dangerouslySetInnerHTML={{ __html: item }}
+                        ></li>
+                      );
+                    })}
+                  </ul>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+}
