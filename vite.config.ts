67ccda98d69096a6bf3869a9765d32a74a968256
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2023-11-11 18:41:25 +0530

    feat: added production url to vite base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env["VERCEL_URL"]
+  base: process.env["VERCEL_ENV"] === 'production' ?
+  'https://kwoc.kossiitkgp.org' :
+  (process.env["VERCEL_URL"]
     ? `https://${process.env["VERCEL_URL"]}`
-    : undefined,
+    : undefined),
 });
