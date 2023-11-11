d052903996179d2f321c02d0aaa1d6585d36044a
Rajiv Harlalka <rajivharlalka009@gmail.com>
2023-11-11 11:58:39 +0530

    feaT: redirect See more button to Testimonial Page
    Signed-off-by: Rajiv Harlalka <rajivharlalka009@gmail.com>

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -8,7 +8,7 @@ export default function Testimonials() {
   let testimonialsDatatoRender = testimonialsData.testimonials.slice(0, 5);
 
   return (
-    <div className="testimonials-container m-auto w-[95%] md:w-[70%] bg-black/50 py-12 md:px-20 rounded-lg  border border-[#FFFFFF]/[0.16]">
+    <div id="testimonial" className="testimonials-container m-auto w-[95%] md:w-[70%] bg-black/50 py-12 md:px-20 rounded-lg  border border-[#FFFFFF]/[0.16]">
       <div className="content-container">
         <h1 className="text-zinc-300 text-center mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl lg:text-7xl">
           What Past Participants Say
@@ -28,7 +28,7 @@ export default function Testimonials() {
           ))}
         </Carousel>
         <div className="sm:w-1/5 m-auto text-center text-zinc-400 font-bold text-xl underline underline-offset-4">
-          <a href="#" className="hover:text-zinc-300">
+          <a href="/testimonials" className="hover:text-zinc-300">
             See More
           </a>
         </div>
