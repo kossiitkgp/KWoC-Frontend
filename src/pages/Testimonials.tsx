980e541b220b261fa7acdd1af6fcbaec7022def1
Rajiv Harlalka <rajivharlalka009@gmail.com>
2023-11-11 11:57:46 +0530

    feat: create the testimonial page
    Signed-off-by: Rajiv Harlalka <rajivharlalka009@gmail.com>

--- src/pages/Testimonials.tsx
+++ src/pages/Testimonials.tsx
@@ -1,6 +1,18 @@
+import TestimonialCard from "../components/TestimonialPageCard";
+import testimonialsData from "../data/testimonials.json";
+
 function Testimonials() {
   return (
-    <div className="testimonials">This website is a work in progress.</div>
+    <div className="text-center p-4 m-auto w-[95%] md:w-[70%] py-12">
+      <div className="my-16">
+        <p className="text-2xl mb-6">What past participants say</p>
+        <div className="grid grid-cols-3 gap-3 p-4 ">
+          {testimonialsData.testimonials.map((testimonial, index) => (
+            <TestimonialCard key={index} testimonial={testimonial} />
+          ))}
+        </div>
+      </div>
+    </div>
   );
 }
 
