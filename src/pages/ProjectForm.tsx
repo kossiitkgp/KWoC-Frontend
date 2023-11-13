b189df666a462c6c293929ba5a7d8a1f5f838e38
koss-service <admin@kossiitkgp.org>
2023-11-13 21:37:45 +0000

    Applied prettier formatting

--- src/pages/ProjectForm.tsx
+++ src/pages/ProjectForm.tsx
@@ -12,105 +12,104 @@ function ProjectForm() {
   const [info, setInfo] = useState<string | null>(null);
 
   useEffect(() => {
-	if (!authContext.isAuthenticated) {
-		navigate(ROUTER_PATHS.HOME);
-	}
-	else if (!authContext.isRegistered) {
-		navigate(authContext.formLink);
-	}
-	else if (authContext.userData.type !== "mentor") {
-		navigate(authContext.dashboardLink);
-	}
+    if (!authContext.isAuthenticated) {
+      navigate(ROUTER_PATHS.HOME);
+    } else if (!authContext.isRegistered) {
+      navigate(authContext.formLink);
+    } else if (authContext.userData.type !== "mentor") {
+      navigate(authContext.dashboardLink);
+    }
   });
 
   return (
-	<>
-	  <Form
-		title="Register A Project"
-		error={error}
-		info={info}
-		fields={{
-			name: {
-				field: 'Project Name',
-				required: true,
-				type: 'text',
-				placeholder: 'My Awesome App'
-			},
-			description: {
-				field: 'Description',
-				required: true,
-				type: 'text',
-				placeholder: 'A short description for the project.'
-			},
-			repo_link: {
-				field: 'Repository Link',
-				required: true,
-				type: 'url',
-				placeholder: 'https://github.com/kossiitkgp/KWoC-Frontend'
-			},
-			comm_channel: {
-				field: 'Communication Channel',
-				required: true,
-				type: 'url',
-				placeholder: DISCORD_INVITE
-			},
-			readme_link: {
-				field: 'README Link',
-				required: true,
-				type: 'url',
-				placeholder: 'https://github.com/kossiitkgp/KWoC-Frontend#readme'
-			},
-			tags: {
-				field: 'Tags (Optional)',
-				type: 'text',
-				placeholder: 'javascript,html,css'
-			},
-			secondary_mentor_username: {
-				field: 'Secondary Mentor Username (Optional)',
-				type: 'text',
-				placeholder: 'proffapt'
-			}
-		}}
-		onCancel={() => {
-		  navigate(ROUTER_PATHS.MENTOR_DASHBOARD);
-		}}
-		onSubmit={async (responses) => {
-			setError(null);
-			setInfo(null);
+    <>
+      <Form
+        title="Register A Project"
+        error={error}
+        info={info}
+        fields={{
+          name: {
+            field: "Project Name",
+            required: true,
+            type: "text",
+            placeholder: "My Awesome App",
+          },
+          description: {
+            field: "Description",
+            required: true,
+            type: "text",
+            placeholder: "A short description for the project.",
+          },
+          repo_link: {
+            field: "Repository Link",
+            required: true,
+            type: "url",
+            placeholder: "https://github.com/kossiitkgp/KWoC-Frontend",
+          },
+          comm_channel: {
+            field: "Communication Channel",
+            required: true,
+            type: "url",
+            placeholder: DISCORD_INVITE,
+          },
+          readme_link: {
+            field: "README Link",
+            required: true,
+            type: "url",
+            placeholder: "https://github.com/kossiitkgp/KWoC-Frontend#readme",
+          },
+          tags: {
+            field: "Tags (Optional)",
+            type: "text",
+            placeholder: "javascript,html,css",
+          },
+          secondary_mentor_username: {
+            field: "Secondary Mentor Username (Optional)",
+            type: "text",
+            placeholder: "proffapt",
+          },
+        }}
+        onCancel={() => {
+          navigate(ROUTER_PATHS.MENTOR_DASHBOARD);
+        }}
+        onSubmit={async (responses) => {
+          setError(null);
+          setInfo(null);
 
-			console.log(responses)
+          console.log(responses);
 
-			try {
-				const res = await makeRequest(
-					'project',
-					'post',
-					{
-						...responses,
-						secondary_mentor_username: responses.secondary_mentor_username ?? '',
-						tags: responses.tags !== "" ? responses.tags.split(',') : [],
-						mentor_username: authContext.userData.username
-					},
-					authContext.jwt
-				)
+          try {
+            const res = await makeRequest(
+              "project",
+              "post",
+              {
+                ...responses,
+                secondary_mentor_username:
+                  responses.secondary_mentor_username ?? "",
+                tags: responses.tags !== "" ? responses.tags.split(",") : [],
+                mentor_username: authContext.userData.username,
+              },
+              authContext.jwt,
+            );
 
-				if (res.is_ok) {
-					navigate(ROUTER_PATHS.MENTOR_DASHBOARD);
-					return true;
-				}
-				else {
-					setError(`${res.response.status_code} Error: ${res.response.message}`);
-					return false;
-				}
-			}
-			catch (e) {
-				console.log(e);
-				setError('An unexpected error occurred.');
+            if (res.is_ok) {
+              navigate(ROUTER_PATHS.MENTOR_DASHBOARD);
+              return true;
+            } else {
+              setError(
+                `${res.response.status_code} Error: ${res.response.message}`,
+              );
+              return false;
+            }
+          } catch (e) {
+            console.log(e);
+            setError("An unexpected error occurred.");
 
-				return false;
-			}
-		}}
-	  />
-	</>
+            return false;
+          }
+        }}
+      />
+    </>
   );
 }
 
