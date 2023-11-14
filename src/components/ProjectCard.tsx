8d3b2c0d3e93b9ec72fb01310de90583bc67406f
Rohan Barsagade <rbarsagade101@gmail.com>
2023-11-15 02:14:47 +0530

    feat: Project page cards new ui

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,6 +1,28 @@
-import { IProject } from "../util/types";
+import { IProject, IProjectTags } from "../util/types";
+import projectTags from "../data/projectTags.json";
 
-function ProjectCard({ name, description, tags, mentor }: IProject) {
+function ProjectCard({
+  name,
+  description,
+  tags,
+  mentor,
+  comm_channel,
+  repo_link,
+  isStudentView,
+}: IProject & { isStudentView: boolean }) {
+  function getColor(tag: string) {
+    if (tag in projectTags) {
+      return (projectTags as IProjectTags)[tag];
+    } else {
+      const randomColorIndex = Math.floor(
+        Math.random() * Object.keys(projectTags as IProjectTags).length,
+      );
+      const randomKey = Object.keys(projectTags as IProjectTags)[
+        randomColorIndex
+      ];
+      return (projectTags as IProjectTags)[randomKey];
+    }
+  }
   return (
     <>
       <div className="p-4 rounded-md w-full h-full bg-[#0f0f27]">
@@ -24,19 +46,39 @@ function ProjectCard({ name, description, tags, mentor }: IProject) {
             .map((tag, i) => (
               <span
                 key={i}
-                className="px-3 py-1 cursor-pointer text-s font-bold rounded-md bg-slate-700"
+                style={{ backgroundColor: getColor(tag) }}
+                className="px-3 py-1 cursor-pointer text-xs font-bold rounded-md"
               >
                 {tag}
               </span>
             ))}
         </div>
         <div className="flex justify-center gap-2 align-bottom">
-          <button className="px-4 py-2 rounded-md bg-blue-950 hover:bg-blue-900 text-lg font-bold flex justify-center items-center">
+          <button
+            onClick={() => window.open(repo_link, "_blank")}
+            className="px-4 py-2 rounded-md bg-blue-950 hover:bg-blue-900 text-lg font-bold flex justify-center items-center"
+          >
             View Project
           </button>
-          <button className="px-4 py-2 rounded-md bg-blue-950 hover:bg-blue-900 text-lg font-bold flex justify-center items-center">
-            Join Channel
-          </button>
+          {isStudentView ? (
+            <button
+              onClick={() => {
+                window.open(comm_channel, "_blank");
+              }}
+              className="px-4 py-2 rounded-md bg-blue-950 hover:bg-blue-900 text-lg font-bold flex justify-center items-center"
+            >
+              Join Channel
+            </button>
+          ) : (
+            <button
+              onClick={() => {
+                // TODO
+              }}
+              className="px-4 py-2 rounded-md bg-blue-950 hover:bg-blue-900 text-lg font-bold flex justify-center items-center"
+            >
+              Edit Project
+            </button>
+          )}
         </div>
       </div>
     </>
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -59,9 +59,9 @@ function Projects() {
       </div>
 
       {error && <p>{error}</p>}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 px-8 items-start">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 max-w-7xl gap-4 px-8 items-start">
         {searchResults.map((project, i) => (
-          <ProjectCard key={i} {...project} />
+          <ProjectCard key={i} {...project} isStudentView={true} />
         ))}
       </div>
     </div>
