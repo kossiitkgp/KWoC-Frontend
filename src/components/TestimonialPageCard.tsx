ebc811619c794c1eca3de73697ac63cf21a6002b
Rohan Barsagade <rbarsagade101@gmail.com>
2023-11-14 22:53:47 +0530

    testimonials

--- src/components/TestimonialPageCard.tsx
+++ src/components/TestimonialPageCard.tsx
@@ -14,29 +14,28 @@ export default function TestimonialCard(props: TestimonialCardProps) {
   const { name, role, quote, imageLink, blogLink } = props.testimonial;
 
   return (
-    <div className=" px-4 py-4 border border-[#FFFFFF]/[0.16] rounded-lg">
-      <div className="w-full flex mb-4 items-center ">
-        <div className="overflow-hidden rounded-full w-10 h-10 border b">
+    <div className=" px-4 py-4 bg-[#0f0f27] rounded-lg">
+      <div className="w-full flex mb-8 items-center gap-4">
+        <div className="overflow-hidden  rounded-full w-14 h-14">
           <img src={imageLink} alt={name} />
         </div>
-        <div className="flex-grow pl-3">
-          <h6 className="font-bold text-sm uppercase ">{name}</h6>
-          <p className="text-zinc-300 text-sm mb-4">{role}</p>
+        <div className="text-left">
+          <h6 className="font-bold text-lg uppercase ">{name}</h6>
+          <p className="text-zinc-500 text-base font-semibold">{role}</p>
         </div>
-        <a href={blogLink} target="_blank" rel="noopener noreferrer">
-          <div className="w-fit m-auto px-2 py-1 bg-white/20 rounded-lg hover:bg-white/10 hover:text-black hover:shadow-md transition-all duration-300 ease-in-out">
-            <p className="text-white text-center font-bold">Read Full Blog</p>
-          </div>
-        </a>
-       
       </div>
-      <div className="w-full">
-        <p className="text-sm leading-tight">
+      <div className="w-full mb-4">
+        <p className="text-base leading-tight">
           <span className="text-lg leading-none italic font-bold  mr-1">"</span>
           {quote}
           <span className="text-lg leading-none italic font-bold ml-1">"</span>
         </p>
       </div>
+      <a href={blogLink} target="_blank" rel="noopener noreferrer">
+        <div className="w-fit m-auto px-2 py-1 bg-blue-950 hover:bg-blue-900 rounded-lg  hover:text-black hover:shadow-md transition-all duration-300 ease-in-out">
+          <p className="text-white text-center font-bold">Read Full Blog</p>
+        </div>
+      </a>
     </div>
   );
 }
--- src/pages/Testimonials.tsx
+++ src/pages/Testimonials.tsx
@@ -3,10 +3,32 @@ import testimonialsData from "../data/testimonials.json";
 
 function Testimonials() {
   return (
-    <div className="text-center p-4 m-auto w-[95%] md:w-[70%] py-12">
-      <div className="my-16">
-        <p className="text-2xl mb-6">What past participants say</p>
-        <div className="grid grid-cols-3 gap-3 p-4 ">
+    <div className="text-center pt-24 m-auto w-[95%] md:w-[70%]">
+      <section className="px-16 my-8 mx-0">
+        <h1 className="font-bold text-3xl sm:text-5xl mb-8 text-center">
+          Testimonials
+        </h1>
+        <p className="text-xl font-semibold text-center">
+          What past participants say
+        </p>
+      </section>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 p-4 ">
+        {testimonialsData.testimonials.map((testimonial, index) => (
+          <TestimonialCard key={index} testimonial={testimonial} />
+        ))}
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 px-8 items-start">
+        <div className="grid gap-3 place-items-start">
+          {testimonialsData.testimonials.map((testimonial, index) => (
+            <TestimonialCard key={index} testimonial={testimonial} />
+          ))}
+        </div>
+        <div className="grid gap-3 place-items-start">
+          {testimonialsData.testimonials.map((testimonial, index) => (
+            <TestimonialCard key={index} testimonial={testimonial} />
+          ))}
+        </div>
+        <div className="grid gap-3 place-items-start">
           {testimonialsData.testimonials.map((testimonial, index) => (
             <TestimonialCard key={index} testimonial={testimonial} />
           ))}
