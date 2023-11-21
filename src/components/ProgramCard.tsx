b5acc0d9ea2e748657037cbf7a7018c1ab732db1
Ranvir <ranvirsingh.9693@gmail.com>
2023-11-21 15:05:25 +0530

    Made UI changes as requested

--- src/components/ProgramCard.tsx
+++ src/components/ProgramCard.tsx
@@ -17,31 +17,31 @@ function ProgramCard({
 }: Program) {
   return (
     <div className="p-8 rounded-md bg-[#2a2a2aa3] col-span-1">
-      <h3 className="font-semibold text-3xl text-center">{year}</h3>
+      <h3 className="font-semibold text-5xl text-center text-white">{year}</h3>
       <div className="flex gap-8 mt-8 justify-center">
         <div >
-          <p className="font-bold">Participants</p>
-          <h6 className="text-2xl font-bold ">{participants}+</h6>
+          <p className="font-bold text-primary">Participants</p>
+          <h6 className="text-2xl font-bold text-white">{participants}+</h6>
         </div>
         <div>
-          <p className="font-bold">PR's</p>
-          <h6 className="text-2xl font-bold ">{prs}+</h6>
+          <p className="font-bold text-primary">PR's</p>
+          <h6 className="text-2xl font-bold text-white">{prs}+</h6>
         </div>
         {projects ? (
           <div>
-            <p className="font-bold">Projects</p>
-            <h6 className="text-2xl font-bold ">{projects}+</h6>
+            <p className="font-bold text-primary">Projects</p>
+            <h6 className="text-2xl font-bold text-white">{projects}+</h6>
           </div>
         ) : (
           <div>
-            <p className="font-bold">Lines of Code</p>
-            <h6 className="text-2xl font-bold ">{locs}M+</h6>
+            <p className="font-bold text-primary">Lines of Code</p>
+            <h6 className="text-2xl font-bold text-white">{locs}M+</h6>
           </div>
         )}
       </div>
 
       <a
-        className="flex justify-center bg-[#ffffff] text-black mt-8 mx-auto p-2 md:w-9/12 rounded-md text-xl"
+        className="flex font-bold justify-center bg-primary text-white mt-8 mx-auto p-2 md:w-9/12 rounded-md text-xl"
         href={link}
         target="_blank"
       >
