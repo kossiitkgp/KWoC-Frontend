9575f7e86ce6156f84c64cb85337422e80a6496a
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2023-11-24 16:06:57 +0530

    fix: set isRegistered in onRegister

--- src/util/auth.tsx
+++ src/util/auth.tsx
@@ -164,6 +164,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
     const newUserAuth = {
       ...userAuth,
       userData,
+      isRegistered: true
     };
 
     updateAuth(newUserAuth);
