f4370d7ba41345487659a8f8bbce33eb28436c80
Ranvir <ranvirsingh.9693@gmail.com>
2023-11-27 14:16:16 +0530

    added responsive padding to the cards

--- src/components/ProgramCard.tsx
+++ src/components/ProgramCard.tsx
@@ -16,9 +16,9 @@ function ProgramCard({
   link,
 }: Program) {
   return (
-    <div className="p-8 rounded-md bg-[#2a2a2aa3] col-span-1">
+    <div className="p-4 md:p-8 rounded-md bg-[#2a2a2aa3] col-span-1">
       <h3 className="font-semibold text-5xl text-center text-white">{year}</h3>
-      <div className="flex gap-8 mt-8 justify-center">
+      <div className="flex gap-5 md:gap-8 mt-8 justify-center">
         <div>
           <p className="font-bold text-blue-300">Participants</p>
           <h6 className="text-2xl font-bold text-white">{participants}+</h6>
