cac2756e6b7a56b1833efd98ca04f5b50ad4f5fb
Asmit Deb <47671715+asmitdeb@users.noreply.github.com>
2023-11-28 22:14:30 +0530

    used let

--- src/util/shuffle.ts
+++ src/util/shuffle.ts
@@ -1,10 +1,10 @@
 // function to randomly shuffle an array
 export function shuffle(array: any[]):any[] {
-    const arraycopy: any[]= [];
+    let arraycopy: any[]= [];
     array.forEach((val: any) => arraycopy.push(Object.assign({}, val)));
-    for (var i = arraycopy.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp:any = arraycopy[i];
+    for (let i = arraycopy.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp:any = arraycopy[i];
         arraycopy[i] = arraycopy[j];
         arraycopy[j] = temp;
     }
