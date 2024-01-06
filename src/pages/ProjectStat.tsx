219f9363ab8d91bb97e57bc349860d086a9cb046
Pranjal Singh <pranjaldevsingh@gmail.com>
2024-01-06 15:30:19 +0530

    Update ProjectStat.tsx

--- src/pages/ProjectStat.tsx
+++ src/pages/ProjectStat.tsx
@@ -2,17 +2,13 @@ import { useEffect, useState } from "react";
 import { makeRequest } from "../util/backend";
 import { IEndpointTypes } from "../util/types";
 import { useAuthContext } from "../util/auth";
-import { useParams } from "react-router-dom";
 function ProjectStats() {
    
   const authContext = useAuthContext();
   const [projectStats, setProjectStats] = useState<
     IEndpointTypes["stats/projects"]["response"] | null
   >(null);
-  const [error, setError] = useState<string | null>(null);
-
-  // Access the project ID from the route parameters
-  const { } = useParams();
+  ;
 
   useEffect(() => {
     // Make a request to the specific project stats endpoint
@@ -21,14 +17,8 @@ function ProjectStats() {
         
         if (response.is_ok) {
           setProjectStats(response.response);
-        } else {
-          setError("Error fetching project stats.");
-          console.log(response.response);
-        }
-      })
-      .catch((e) => {
-        setError("Error fetching project stats.");
-        console.log(e);
+        } 
+       
       });
   }, []);  
 
@@ -37,8 +27,8 @@ function ProjectStats() {
       <h1 className="font-display text-5xl md:text-7xl font-bold text-center">
         Project Stats
       </h1>
-      {error !== null ? (
-        <p className="text-center text-red-500">{error}</p>
+     (
+        <p className="text-center text-red-500"></p>
       ) : projectStats !== null ? (
         <div className="max-w-7xl px-8 py-4">
           <table className="min-w-full">
