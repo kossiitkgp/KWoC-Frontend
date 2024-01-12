d4a3c1cfb4e57eb2601a128178fbf9e958c4ea5b
Nafisa Parvin <parvinnafisa21@gmail.com>
2024-01-12 21:30:24 +0530

    made blogform a component

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ import PastProgramsPage from "./pages/PastProgramsPage";
 import StudentDashboard from "./pages/StudentDashboard";
 import RegistrationForm from "./pages/RegistrationForm";
 import NotFoundPage from "./pages/404";
-import BlogForm from "./pages/BlogForm";
+import BlogForm from "./components/BlogForm";
 
 function App() {
   return (
