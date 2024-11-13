9eb56d55e20d6668d4899a914841004a2fef09ae
Bikram Ghuku <bikramghuku05@gmail.com>
2024-11-13 10:31:52 +0530

    fix: deployment issues

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import TestimonialCard from "./TestimonialCard";
 import testimonialsData from "../data/testimonials.json";
 import "../styles/Testimonials.css";
