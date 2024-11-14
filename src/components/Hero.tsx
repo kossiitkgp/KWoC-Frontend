29cec91bbe12f6fc6ec29c7b897c0a2068b8d907
Tegan Jain <teganjain527@gmail.com>
2024-11-15 02:13:37 +0530

    effects: reduce snowflakes and remove rudolph

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -15,8 +15,8 @@ export const HeroComponent = () => {
           <h1>Kharagpur <span className='yellow-text'>Winter<br />of Code</span> 2024</h1>
         </div>
         <div className="hero-button-container">
-        <button className="hero-button"> <a href="https://drive.google.com/file/d/1gc2NWeMDA3ZFzH4RWVspuerduRdfY9yu/view?usp=drive_link" target="_blank">Student Manual </a></button>
-        <button className="hero-button"> <a href="https://drive.google.com/file/d/1XUGfHygE3_KYX_qhcpvw8jhKLlcRkzHP/view?usp=drive_link" target="_blank">Mentor Manual </a></button>
+          <button className="hero-button"> <a href="https://drive.google.com/file/d/1gc2NWeMDA3ZFzH4RWVspuerduRdfY9yu/view?usp=drive_link" target="_blank">Student Manual </a></button>
+          <button className="hero-button"> <a href="https://drive.google.com/file/d/1XUGfHygE3_KYX_qhcpvw8jhKLlcRkzHP/view?usp=drive_link" target="_blank">Mentor Manual </a></button>
         </div>
       </div>
       <div className="hero-images">
