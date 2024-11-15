f3e2bfb3aedead3864dc7f0d2394de328b11d079
Tegan Jain <teganjain527@gmail.com>
2024-11-15 18:05:21 +0530

    add Discord server link to hero section and fix that annoying blue font in the buttons

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,8 @@ import hook from '../assets/igloo-scroll-hook-edit.png';
 import sign from '../assets/sign-post.png';
 import "../styles/Hero.css";
 import "../styles/App.css";
-import { MENTOR_MANUAL_LINK, STUDENT_MANUAL_LINK } from '../util/constants';
+import { FaDiscord } from 'react-icons/fa';
+import { DISCORD_INVITE, MENTOR_MANUAL_LINK, STUDENT_MANUAL_LINK } from '../util/constants';
 
 export const HeroComponent = () => {
   return (
@@ -16,8 +17,9 @@ export const HeroComponent = () => {
           <h1>Kharagpur <span className='yellow-text'>Winter<br />of Code</span> 2024</h1>
         </div>
         <div className="hero-button-container">
-          <button className="hero-button"> <a href={STUDENT_MANUAL_LINK} target="_blank">Student Manual </a></button>
-          <button className="hero-button"> <a href={MENTOR_MANUAL_LINK} target="_blank">Mentor Manual </a></button>
+          <a href={STUDENT_MANUAL_LINK} target="_blank"><button className='hero-button'>Student Manual</button> </a>
+          <a href={MENTOR_MANUAL_LINK} target="_blank"><button className='hero-button'>Mentor Manual</button> </a>
+          <a href={DISCORD_INVITE} target="_blank"><button className='discord-button'><FaDiscord className='discord-icon' /><p>Discord Server</p></button> </a>
         </div>
       </div>
       <div className="hero-images">
