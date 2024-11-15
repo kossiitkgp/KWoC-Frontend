ef02609ce183ad000b767ec17e60b110bbb3a8ea
Tegan Jain <teganjain527@gmail.com>
2024-11-16 00:08:25 +0530

    change the why kwoc cards to have different content how tf did no one notice they were the same???

--- src/components/WhyKWoC.tsx
+++ src/components/WhyKWoC.tsx
@@ -14,8 +14,8 @@ export const WhyKWoC = () => {
       </div>
       <div className="why-cards">
         <WhyCard title="Intro to Open Source" description="Get started with open source collaboration. Hone programmer skills with Git and GitHub. Make meaningful contributions guided by mentors. Build a portfolio showcasing your talents." />
-        <WhyCard title="Intro to Open Source" description="Get started with open source collaboration. Hone programmer skills with Git and GitHub. Make meaningful contributions guided by mentors. Build a portfolio showcasing your talents." />
-        <WhyCard title="Intro to Open Source" description="Get started with open source collaboration. Hone programmer skills with Git and GitHub. Make meaningful contributions guided by mentors. Build a portfolio showcasing your talents." />
+        <WhyCard title="Prepare for GSoC" description="Research organizations and connect with mentors. Create quality proposals and sharpen skills. Gain insider tips to boost your GSoC chances. Open doors through impressive abilities." />
+        <WhyCard title="Earn a Certificate" description="Receive a certificate upon successful completition of the program. Boost your professional profile and validate your programming skills with this credential. Showcase your expertise." />
       </div>
     </div>
   )
