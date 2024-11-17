764bec9c12df08d9f8f81887a26550f5fc1ee701
harshit-jain52 <harshitjain0502@gmail.com>
2024-11-18 04:54:23 +0530

    styles: snowflakes on only landing page; top left logo is not a link now

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,11 +4,24 @@ import { AboutSection } from "../components/About";
 import { WhyKWoC } from "../components/WhyKWoC";
 import "../styles/Santa.css";
 import Testimonials from "../components/Testimonials";
+import { Snowfall } from "react-snowfall";
 
 function Home() {
-
   return (
     <div>
+      <Snowfall // the image can be edited into anything we want, feel free to change
+        snowflakeCount={75}
+        speed={[0.5, 1]} // array takes [min, max]
+        wind={[-0.5, 0.5]}
+        radius={[2, 4]}
+        style={{
+          position: "fixed",
+          width: "100vw",
+          height: "100vh",
+          opacity: 0.5,
+          zIndex: 0,
+        }}
+      />
       <HeroComponent />
       <AboutSection />
       <WhyKWoC />
