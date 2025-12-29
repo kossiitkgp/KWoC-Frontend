18bed4461f4c2fc7997100ef223262536d6e8e7d
Dipam Sen <dipamsen.fp@gmail.com>
2025-12-29 17:12:03 +0530

    Revert "remove comment"
    This reverts commit 7a5c6715a2d9fc6a5e069c66861a974f6d3435e8.

--- src/pages/ProjectForm.tsx
+++ src/pages/ProjectForm.tsx
@@ -124,13 +124,14 @@ function ProjectForm(props: { isEditing?: boolean }) {
             setError(null);
             setInfo(null);
 
+            // REQUIRE minimum 3 tags (NO empty tags allowed)
             const tagsArray = responses.tags
               .split(",")
               .map((t) => t.trim())
               .filter((t) => t.length > 0);
 
             if (tagsArray.length < 3) {
-              setError("At least 3 tags are required.");
+              setError("Minimum 3 tags required! (e.g: javascript, html, css)");
               return false;
             }
 
